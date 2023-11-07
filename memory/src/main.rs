526d1032c6279cf721ea136cdf7dd342b5103fe5
jamestheengineer <jamestheengineer@gmail.com>
2023-11-06 19:09:23 -0500

    More memory things.

--- memory/src/main.rs
+++ memory/src/main.rs
@@ -6,4 +6,17 @@ fn main() {
 
     println!("{}", s); // This will print `hello, world!`
 
+    let s1 = String::from("hello");
+    let s2 = s1;
+    println!("{}", s2);
+
+    // wow
+    //println!("{}, world!", s1);
+
+    // This is OK, though
+    let x = 5;
+    let y = x;
+
+    println!("x = {}, y = {}", x, y);
+
 }
