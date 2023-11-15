f83793057dc1b89a0211cb18450adf5b7c6b9684
jamestheengineer <jamestheengineer@gmail.com>
2023-11-14 21:23:17 -0500

    Structs

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -12,6 +12,12 @@ fn main() {
         email: String::from("another@example.com"),
         ..user1
     };
+
+    struct Color(i32, i32, i32);
+    struct Point(i32, i32, i32);
+
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
 }
 
 struct User {
