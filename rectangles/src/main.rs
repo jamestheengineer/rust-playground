14b20c003e300189d157fbfd9b67101b36750b55
jamestheengineer <jamestheengineer@gmail.com>
2023-11-17 18:32:45 -0500

    better rects

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -6,8 +6,38 @@ fn main() {
         "The area of the rectangle is {} square pixels.",
         area(width1, height1)
     );
+
+    let rect1 = (30, 50);
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area2(rect1)
+    );
+
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area3(&rect1)
+    );
 }
 
 fn area(width: u32, height: u32) -> u32 {
     width * height
+}
+
+fn area2(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
+}
+
+fn area3(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
+}
+
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
