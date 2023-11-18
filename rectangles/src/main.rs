35331406b06c1219a8129bb6dd9cebbe1799684d
jamestheengineer <jamestheengineer@gmail.com>
2023-11-18 10:42:40 -0500

    Debug attribute

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -19,6 +19,8 @@ fn main() {
         height: 50,
     };
 
+    println!("rect1 is {:?} {:#?}", rect1, rect1);
+
     println!(
         "The area of the rectangle is {} square pixels.",
         area3(&rect1)
@@ -37,6 +39,7 @@ fn area3(rectangle: &Rectangle) -> u32 {
     rectangle.width * rectangle.height
 }
 
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
