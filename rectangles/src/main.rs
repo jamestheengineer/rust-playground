fb26d14e059180b2e33a703a1cfe7d661aa2d1a3
jamestheengineer <jamestheengineer@gmail.com>
2023-11-18 10:45:38 -0500

    Debug macro

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -14,10 +14,12 @@ fn main() {
         area2(rect1)
     );
 
+    let scale = 2;
     let rect1 = Rectangle {
-        width: 30,
+        width: dbg!(30 * scale),
         height: 50,
     };
+    dbg!(&rect1);
 
     println!("rect1 is {:?} {:#?}", rect1, rect1);
 
