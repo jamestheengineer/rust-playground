f9e82c6eef26fa833be936a4bb2683b31b5b289d
jamestheengineer <jamestheengineer@gmail.com>
2023-11-22 17:41:48 -0500

    Associated functions

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -47,6 +47,9 @@ fn main() {
 
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
     println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let sq = Rectangle::square(3);
+    println!("This is a associated function, not a method {:#?}", sq)
 }
 
 fn area(width: u32, height: u32) -> u32 {
@@ -72,5 +75,11 @@ impl Rectangle {
     }
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
+    }
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
     }
 }
