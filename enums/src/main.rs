fc21504e50fb996aa2e70c29024918ccb2081113
jamestheengineer <jamestheengineer@gmail.com>
2023-11-27 20:15:32 -0500

    Match statement and Enum with data

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -13,6 +13,17 @@ fn main() {
 
     value_in_cents(Coin::Quarter(UsState::Alaska));
 
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
 }
 
 #[derive(Debug)] // so we can inspect the state in a minute
