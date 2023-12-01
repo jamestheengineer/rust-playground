7b8deb0992e79af2da538bbfa72ffad70506e219
jamestheengineer <jamestheengineer@gmail.com>
2023-11-30 19:02:28 -0500

    Match statement and Enum with data

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -17,7 +17,18 @@ fn main() {
     let six = plus_one(five);
     let none = plus_one(None);
 
+    let dice_roll = 9;
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        other => move_player(other),
+    }
+
 }
+
+fn add_fancy_hat() {}
+fn remove_fancy_hat() {}
+fn move_player(num_spaces: u8) {}
 
 fn plus_one(x: Option<i32>) -> Option<i32> {
     match x {
