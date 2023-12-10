7df6e49bcd77e544c8de97e034107b6a2363b7e2
jamestheengineer <jamestheengineer@gmail.com>
2023-12-10 11:54:09 -0500

    use statement

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -14,9 +14,10 @@ mod front_of_house {
     }
 }
 
+use crate::front_of_house::hosting;
 pub fn eat_at_restaurant() {
     // Absolute path
-    crate::front_of_house::hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
 
     // Relative path
     front_of_house::hosting::add_to_waitlist();
