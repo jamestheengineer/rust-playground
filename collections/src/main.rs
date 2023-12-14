674b56d42b920d3753ff3fd48935920fcadec162
jamestheengineer <jamestheengineer@gmail.com>
2023-12-14 18:42:55 -0500

    Vector but with enum

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -22,7 +22,47 @@
 
     let v = vec![1, 2, 3, 4, 5];
 
-    let does_not_exist = &v[100];
+    // let does_not_exist = &v[100];
     let does_not_exist = v.get(100);
 
+    let v = vec![100, 32, 57];
+    for i in &v {
+        println!("{i}");
+    }
+
+    let mut v = vec![100, 32, 57];
+    for i in &mut v {
+        *i += 50;
+        println!("{i}");
+    }
+
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
+    }
+
+    use std::fmt;
+
+    impl fmt::Display for SpreadsheetCell {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            SpreadsheetCell::Text(s) => write!(f, "{}", s),
+            SpreadsheetCell::Int(i) => write!(f, "{}", i),
+            SpreadsheetCell::Float(num) => write!(f, "{}", num),
+        }
+    }
 }
+
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
+    for i in &row {
+        println!("{}", i);
+    }
+
+}
+
+
