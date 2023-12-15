3199c868cf617067da50b26e9be969290da82e2e
jamestheengineer <jamestheengineer@gmail.com>
2023-12-15 18:45:02 -0500

    Strings

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -63,6 +63,16 @@
         println!("{}", i);
     }
 
+    let mut s = String::new();
+    let data = "initial contents";
+
+    let s = data.to_string();
+
+    // the method also works on a literal directly:
+    let s = "initial contents".to_string();
+
+
+
 }
 
 
