292ce3ccfe1a48aaf90bed1a98dab8474bc6c7fd
jamestheengineer <jamestheengineer@gmail.com>
2023-12-16 09:43:21 -0500

    Strings

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -71,6 +71,19 @@
     // the method also works on a literal directly:
     let s = "initial contents".to_string();
 
+    let hello = String::from("السلام عليكم");
+    let hello = String::from("Dobrý den");
+    let hello = String::from("Hello");
+    let hello = String::from("שָׁלוֹם");
+    let hello = String::from("नमस्ते");
+    let hello = String::from("こんにちは");
+    let hello = String::from("안녕하세요");
+    let hello = String::from("你好");
+    let hello = String::from("Olá");
+    let hello = String::from("Здравствуйте");
+    let hello = String::from("Hola");
+
+
 
 
 }
