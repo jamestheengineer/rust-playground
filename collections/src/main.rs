7671192d888a98aeeea6f975f6ff9fb9cea47c47
jamestheengineer <jamestheengineer@gmail.com>
2023-12-17 10:16:33 -0500

    Strings

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -83,6 +83,30 @@
     let hello = String::from("Здравствуйте");
     let hello = String::from("Hola");
 
+    let mut s = String::from("foo");
+    s.push_str("bar");
+
+    let mut s1 = String::from("foo");
+    let s2 = "bar";
+    s1.push_str(s2);
+    println!("s2 is {s2}");
+
+    let mut s = String::from("lo");
+    s.push('l');
+
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("world!");
+    let s3 = s1 + &s2; // note s1 has been moved here and can no longer be used
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+    let s = s1 + "-" + &s2 + "-" + &s3; // remember that s1 is gone
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+    let s = format!("{s1}-{s2}-{s3}"); // s1 is not gone when using format!
 
 
 
