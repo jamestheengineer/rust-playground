b08c15c24ad85434fdee0dbe311c41522af94707
jamestheengineer <jamestheengineer@gmail.com>
2023-12-24 10:14:33 -0500

    More panic.

--- errors/src/main.rs
+++ errors/src/main.rs
@@ -1,6 +1,12 @@
+use std::fs::File;
 fn main() {
     // panic!("crash and burn");
-    let v = vec![1, 2, 3];
+    // let v = vec![1, 2, 3];
+    // v[99];
 
-    v[99];
+    let greeting_file_result = File::open("hello.txt");
+    let greeting_file = match greeting_file_result {
+        Ok(file) => file,
+        Err(error) => panic!("Problem opening the file: {:?}", error),
+    };
 }
