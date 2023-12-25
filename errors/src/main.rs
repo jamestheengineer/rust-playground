b87b4c7ce893cccdf1dc5ad7fb18e67cb47dab38
jamestheengineer <jamestheengineer@gmail.com>
2023-12-25 09:52:34 -0500

    Handling errors

--- errors/src/main.rs
+++ errors/src/main.rs
@@ -1,12 +1,38 @@
 use std::fs::File;
+use std::io::ErrorKind;
+
 fn main() {
     // panic!("crash and burn");
     // let v = vec![1, 2, 3];
     // v[99];
 
     let greeting_file_result = File::open("hello.txt");
+
     let greeting_file = match greeting_file_result {
         Ok(file) => file,
-        Err(error) => panic!("Problem opening the file: {:?}", error),
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => {
+                panic!("Problem opening the file: {:?}", other_error);
+            }
+        },
     };
+
+    // Using unwrap functions instead to minimize matching statements
+    let greeting_file = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Problem creating the file: {:?}", error);
+            })
+        } else {
+            panic!("Problem opening the file: {:?}", error);
+        }
+    });
+
+    // Better way of telling dev/user that something ain't right.
+    let greeting_file = File::open("hello2.txt")
+        .expect("hello.txt should be included in this project");
 }
