e5918c6a7e1a79f7cfdde24128c180e6544f5549
jamestheengineer <jamestheengineer@gmail.com>
2023-12-26 10:48:51 -0500

    Handling errors by propagation

--- errors/src/main.rs
+++ errors/src/main.rs
@@ -1,5 +1,6 @@
 use std::fs::File;
 use std::io::ErrorKind;
+use std::io::{self, Read};
 
 fn main() {
     // panic!("crash and burn");
@@ -35,4 +36,28 @@ fn main() {
     // Better way of telling dev/user that something ain't right.
     let greeting_file = File::open("hello2.txt")
         .expect("hello.txt should be included in this project");
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let username_file_result = File::open("hello.txt");
+
+    let mut username_file = match username_file_result {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut username = String::new();
+
+    match username_file.read_to_string(&mut username) {
+        Ok(_) => Ok(username),
+        Err(e) => Err(e),
+    }
+}
+
+fn read_username_from_file_short() -> Result<String, io::Error> {
+    let mut username = String::new();
+
+    File::open("hello.txt")?.read_to_string(&mut username)?;
+
+    Ok(username)
 }
