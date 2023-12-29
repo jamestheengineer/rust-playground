fb5fb4978cd1414ab96b86f1d526d6fd4a267abc
jamestheengineer <jamestheengineer@gmail.com>
2023-12-29 17:36:11 -0500

    Define a generic function

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -22,7 +22,7 @@ fn main() {
     println!("The largest number is {}", result);
 }
 
-fn largest(list: &[i32]) -> &i32 {
+fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
     let mut largest = &list[0];
 
     for item in list {
