19f14b8bd0dc47a8f2978baef6d96ba9103b2a33
jamestheengineer <jamestheengineer@gmail.com>
2023-12-30 10:52:47 -0500

    Generics in structs and impl functions

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -20,6 +20,9 @@ fn main() {
 
     let result = largest(&number_list);
     println!("The largest number is {}", result);
+
+    let integer = Point { x: 5, y: 10 };
+    let float = Point { x: 1.0, y: 4.0 };
 }
 
 fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
@@ -32,4 +35,19 @@ fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
     }
 
     largest
+}
+
+struct Point<T> {
+    x: T,
+    y: T,
+}
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+impl Point<f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
 }
