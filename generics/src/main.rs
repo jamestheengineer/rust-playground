4093a522dc2eac0d609da178e14222e62610a5a1
jamestheengineer <jamestheengineer@gmail.com>
2024-01-02 18:11:59 -0500

    Lifetimes in structs

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -35,7 +35,17 @@ fn main() {
 
     let result = longest(string1.as_str(), string2);
     println!("The longest string is {}", result);
-}                           // ----------+
+
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
+}                          // ----------+
+
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
 
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {
