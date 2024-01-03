20b2818a728e6e342b94398040cf67761078c52a
jamestheengineer <jamestheengineer@gmail.com>
2024-01-03 17:23:39 -0500

    Lifetimes summary

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -47,6 +47,23 @@ struct ImportantExcerpt<'a> {
     part: &'a str,
 }
 
+use std::fmt::Display;
+fn longest_with_an_announcement<'a, T>(
+    x: &'a str,
+    y: &'a str,
+    ann: T,
+) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {
         x
