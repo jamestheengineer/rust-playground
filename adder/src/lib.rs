bbc84618aa67ba018c41423f052f34dd34a0db10
jamestheengineer <jamestheengineer@gmail.com>
2024-01-05 17:57:16 -0500

    Create some tests

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -7,8 +7,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn exploration() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn another() {
+        panic!("Make this test fail");
     }
 }
