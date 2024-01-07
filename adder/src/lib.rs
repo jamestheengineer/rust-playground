4e5901dea9749bae7ee19b2a09331e376de58ca6
jamestheengineer <jamestheengineer@gmail.com>
2024-01-07 13:40:15 -0500

    assert_eq and assert_ne

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -38,6 +38,15 @@ mod tests {
 
         assert!(!smaller.can_hold(&larger));
     }
+
+    #[test]
+    fn it_adds_two() {
+        assert_eq!(4, add_two(2));
+    }
+}
+
+pub fn add_two(a: i32) -> i32 {
+    a + 2
 }
 
 #[derive(Debug)]
