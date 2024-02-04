f0332ebedd8def308758bb25f89d977d9e2e9cbf
jamestheengineer <jamestheengineer@gmail.com>
2024-02-04 15:56:33 -0500

    More closures

--- shirt_company/src/main.rs
+++ shirt_company/src/main.rs
@@ -58,4 +58,13 @@ fn main() {
         num
     };
     expensive_closure(100);
+
+    let list = vec![1, 2, 3];
+    println!("Before defining closure: {:?}", list);
+
+    let only_borrows = || println!("From closure: {:?}", list);
+
+    println!("Before calling closure: {:?}", list);
+    only_borrows();
+    println!("After calling closure: {:?}", list);
 }
