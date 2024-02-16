131a59f2ee026e4bf1ada0d80e3c25c22018590c
jamestheengineer <jamestheengineer@gmail.com>
2024-02-16 18:45:56 -0500

    Docs in rust

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,3 +1,8 @@
+//! # My Crate
+//!
+//! `my_crate` is a collection of utilities to make performing certain
+//! calculations more convenient.
+
 mod front_of_house;
 use crate::front_of_house::hosting;
 pub fn eat_at_restaurant() {
