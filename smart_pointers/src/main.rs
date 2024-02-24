010d2531082714ebb699afe6bb483c0b78d7b472
jamestheengineer <jamestheengineer@gmail.com>
2024-02-24 09:19:09 -0500

    Deref trait

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -1,8 +1,7 @@
+
 fn main() {
     let b = Box::new(5);
     println!("b = {}", b);
-
-    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
 
     let x = 5;
     let y = &x;
@@ -11,15 +10,26 @@ fn main() {
     assert_eq!(5, *y);
 
     let x = 5;
-    let y = Box::new(x);
+    let y = MyBox::new(x);
 
     assert_eq!(5, x);
     assert_eq!(5, *y);
 }
 
-enum List {
-    Cons(i32, Box<List>),
-    Nil,
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
 }
 
-use crate::List::{Cons, Nil};
+use std::ops::Deref;
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
