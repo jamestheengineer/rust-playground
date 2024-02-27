e5fbb31712b639f63384665caeb2c1fb7dad6981
jamestheengineer <jamestheengineer@gmail.com>
2024-02-27 18:50:16 -0500

    Reference Counted Smart Pointers

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -1,4 +1,7 @@
 
+use crate::List::{Cons, Nil};
+use std::rc::Rc;
+
 fn main() {
     let b = Box::new(5);
     println!("b = {}", b);
@@ -29,6 +32,16 @@ fn main() {
     println!("CustomSmartPointer created.");
     drop(_e);
     println!("CustomSmartPointer dropped before the end of main.");
+
+    let _a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("count after creating a = {}", Rc::strong_count(&_a));
+    let _b = Cons(3, Rc::clone(&_a));
+    println!("count after creating b = {}", Rc::strong_count(&_a));
+    {
+        let _c = Cons(4, Rc::clone(&_a));
+        println!("count after creating c = {}", Rc::strong_count(&_a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&_a));
 }
 
 struct CustomSmartPointer {
@@ -58,3 +71,11 @@ impl<T> Deref for MyBox<T> {
         &self.0
     }
 }
+
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
+}
+
+
+    
