063701a5b901e174cfbbb38fcaab1e6e76dda4a3
jamestheengineer <jamestheengineer@gmail.com>
2024-03-09 08:45:30 -0500

    Move to closures to make sure data is owned in threads

--- threads/src/main.rs
+++ threads/src/main.rs
@@ -29,4 +29,12 @@ fn main() {
         println!("hi number {} from the main thread!", i);
         thread::sleep(Duration::from_millis(1));
     }
+
+    let v = vec![1, 2, 3];
+
+    let handle = thread::spawn(move || {
+        println!("Here's a vector: {:?}", v);
+    });
+
+    handle.join().unwrap();
 }
