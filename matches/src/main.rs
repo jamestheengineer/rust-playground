fbec32624f0d12e14d619d45eead6f488efb460f
jamestheengineer <jamestheengineer@gmail.com>
2024-03-23 08:14:26 -0400

    More technical matching statements

--- matches/src/main.rs
+++ matches/src/main.rs
@@ -27,4 +27,20 @@ fn main() {
         println!("{}", top);
     }
 
+    let v = vec!['a', 'b', 'c'];
+
+    for (index, value) in v.iter().enumerate() {
+        println!("{} is at index {}", value, index);
+    }
+
+    let (x, y, z) = (1, 2, 3);
+    println!("{}{}{}", x, y, z);
+
+    let point = (3, 5);
+    print_coordinates(&point);
+
+}
+
+fn print_coordinates(&(x, y): &(i32, i32)) {
+    println!("Current location: ({}, {})", x, y);
 }
