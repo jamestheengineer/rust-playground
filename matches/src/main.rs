055f35ff5927b54ab8fd2bd717e7df999a970240
jamestheengineer <jamestheengineer@gmail.com>
2024-03-26 06:58:21 -0400

    Multiple match styles

--- matches/src/main.rs
+++ matches/src/main.rs
@@ -64,6 +64,29 @@ fn main() {
 
     println!("at the end: x = {:?}, y = {y}", x);
 
+    let x = 1;
+
+    match x {
+        1 | 2 => println!("one or two"),
+        3 => println!("three"),
+        _ => println!("anything"),
+    }
+
+    let x = 5;
+
+    match x {
+        1..=5 => println!("one through five"),
+        _ => println!("something else"),
+    }
+
+    let x = 'c';
+
+    match x {
+        'a'..='j' => println!("early ASCII letter"),
+        'k'..='z' => println!("late ASCII letter"),
+        _ => println!("something else"),
+    }
+
 
 }
 
