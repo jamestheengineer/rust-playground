20de3c987cb4212b4c23fe229b759424ca46c6c0
jamestheengineer <jamestheengineer@gmail.com>
2024-03-27 19:32:18 -0400

    Destructuring matches

--- matches/src/main.rs
+++ matches/src/main.rs
@@ -87,7 +87,32 @@ fn main() {
         _ => println!("something else"),
     }
 
+    let p = Point { x: 0, y: 7 };
 
+    let Point { x: a, y: b } = p;
+    assert_eq!(0, a);
+    assert_eq!(7, b);
+
+    // Shorthand
+    let Point { x, y } = p;
+    assert_eq!(0, x);
+    assert_eq!(7, y);
+
+    
+    match p {
+        Point { x, y: 0 } => println!("On the x axis at {x}"),
+        Point { x: 0, y } => println!("On the y axis at {y}"),
+        Point { x, y } => {
+            println!("On neither axis: ({x}, {y})");
+        }
+    }
+
+
+}
+
+struct Point {
+    x: i32,
+    y: i32,
 }
 
 fn print_coordinates(&(x, y): &(i32, i32)) {
