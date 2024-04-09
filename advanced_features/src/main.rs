b012966b50738abdfc904e62a90add1f6e57552e
jamestheengineer <jamestheengineer@gmail.com>
2024-04-08 21:20:39 -0400

    FFI to C code. Unsafe, of course.

--- advanced_features/src/main.rs
+++ advanced_features/src/main.rs
@@ -35,8 +35,16 @@ fn main() {
 
     let _values: &[i32] = unsafe { slice::from_raw_parts_mut(r, 10000) };
 
+    unsafe {
+        println!("Absolute value of -3 according to C: {}", abs(-3));
+    }
 
 }
+
+extern "C" {
+    fn abs(input: i32) -> i32;
+}
+
 
 use std::slice;
 
