38a0f052839b70c54bcacefc01e5180d51d037d9
jamestheengineer <jamestheengineer@gmail.com>
2024-04-13 07:27:15 -0400

    Handling ambiguity in same fn names

--- advanced_features/src/main.rs
+++ advanced_features/src/main.rs
@@ -61,6 +61,39 @@ fn main() {
     println!("Total length in mm is {:?} mm", total_length_mm);
     println!("Total length in m is {:?} m", total_length_m);
 
+    let person = Human;
+    Pilot::fly(&person);
+    Wizard::fly(&person);
+    person.fly();
+
+}
+
+trait Pilot {
+    fn fly(&self);
+}
+
+trait Wizard {
+    fn fly(&self);
+}
+
+struct Human;
+
+impl Pilot for Human {
+    fn fly(&self) {
+        println!("This is your captain speaking.");
+    }
+}
+
+impl Wizard for Human {
+    fn fly(&self) {
+        println!("Up!");
+    }
+}
+
+impl Human {
+    fn fly(&self) {
+        println!("*waving arms furiously*");
+    }
 }
 
 use std::ops::Add;
