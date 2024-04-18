985fc9b7a7fd16e739585a5efb2e2e28c75c15fe
jamestheengineer <jamestheengineer@gmail.com>
2024-04-18 19:32:03 -0400

    More type aliases

--- advanced_features/src/main.rs
+++ advanced_features/src/main.rs
@@ -83,6 +83,16 @@ fn main() {
 
 use std::fmt;
 
+type Result<T> = std::result::Result<T, std::io::Error>;
+pub trait Write {
+    fn write(&mut self, buf: &[u8]) -> Result<usize>;
+    fn flush(&mut self) -> Result<()>;
+
+    fn write_all(&mut self, buf: &[u8]) -> Result<()>;
+    fn write_fmt(&mut self, fmt: fmt::Arguments) -> Result<()>;
+}
+
+
 struct Wrapper(Vec<String>);
 
 impl fmt::Display for Wrapper {
