9379d054318649785a5385d8d89563ea1266321d
jamestheengineer <jamestheengineer@gmail.com>
2024-05-13 18:46:59 -0400

    Get this to compile

--- hello/src/lib.rs
+++ hello/src/lib.rs
@@ -46,7 +46,7 @@ impl ThreadPool {
 
 struct Worker {
   id: usize,
-  thread: thread::JoinHandle<()>,
+  thread: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
@@ -59,7 +59,7 @@ impl Worker {
       job();
     });
 
-    Worker { id, thread }
+    Worker { id, thread: Some(thread), }
   }
 }
 
@@ -68,7 +68,9 @@ impl Drop for ThreadPool {
     for worker in &mut self.workers {
       println!("Shutting down worker {}", worker.id);
 
-      worker.thread.join().unwrap();
+      if let Some(thread) = worker.thread.take() {
+        thread.join().unwrap();
+      }
     }
   }
 }
