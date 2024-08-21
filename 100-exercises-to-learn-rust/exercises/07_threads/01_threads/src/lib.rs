da3d5b997662fc8d0d4aab96cf7259c99f7b023f
jamestheengineer <jamestheengineer@gmail.com>
2024-08-21 10:02:59 -0600

    Threads

--- 100-exercises-to-learn-rust/exercises/07_threads/01_threads/src/lib.rs
+++ 100-exercises-to-learn-rust/exercises/07_threads/01_threads/src/lib.rs
@@ -8,14 +8,22 @@
 // You _could_ pass this test by just returning `v.iter().sum()`,
 // but that would defeat the purpose of the exercise.
 //
-// Hint: you won't be able to get the spawn threads to _borrow_
+// Hint: you won't be able to get the spawned threads to _borrow_
 // slices of the vector directly. You'll need to allocate new
 // vectors for each half of the original vector. We'll see why
 // this is necessary in the next exercise.
 use std::thread;
 
 pub fn sum(v: Vec<i32>) -> i32 {
-    todo!()
+    let mid = v.len() / 2;
+    let (v1, v2) = v.split_at(mid);
+    let v1 = v1.to_vec();
+    let v2 = v2.to_vec();
+
+    let handle1 = thread::spawn(move || v1.into_iter().sum::<i32>());
+    let handle2 = thread::spawn(move || v2.into_iter().sum::<i32>());
+
+    handle1.join().unwrap() + handle2.join().unwrap()
 }
 
 #[cfg(test)]
