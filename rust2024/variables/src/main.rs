1253cf6679d38b93102c850727639049d5f6b858
jamestheengineer <jamestheengineer@gmail.com>
2025-04-14 19:20:51 -0400

    constants

--- rust2024/variables/src/main.rs
+++ rust2024/variables/src/main.rs
@@ -1,7 +1,11 @@
+const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
 fn main() {
     // let x = 5; // Immutable
     let mut x = 5;
     println!("The value of x is: {x}");
     x = 6;
     println!("The value of x is: {x}");
+
+    println!("The value of the constant is {}", THREE_HOURS_IN_SECONDS);
 }
