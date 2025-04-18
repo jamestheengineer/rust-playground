0a45ebbac3da9fc467f72fa76504ff7555ca7aea
jamestheengineer <jamestheengineer@gmail.com>
2025-04-18 18:30:23 -0400

    functions

--- rust2024/variables/src/main.rs
+++ rust2024/variables/src/main.rs
@@ -1,9 +1,22 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let a = [1, 2, 3, 4, 5];
 
-    another_function();
-}
+    println!("Please enter an array index.");
 
-fn another_function() {
-    println!("Another function.");
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!("The value of the element at index {index} is: {element}");
 }
