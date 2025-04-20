edd1b04fd838e93d168ee0bf909ff90b3e50e9aa
jamestheengineer <jamestheengineer@gmail.com>
2025-04-20 12:12:37 -0400

    expressions and statements

--- rust2024/functions/src/main.rs
+++ rust2024/functions/src/main.rs
@@ -1,7 +1,14 @@
 fn main() {
-    another_function(5);
+    print_labeled_measurement(5, 'h');
+
+    let y = {
+        let x = 3;
+        x + 1
+    };
+
+    println!("The value of y is: {y}");
 }
 
-fn another_function(x: i32) {
-    println!("The value of x is: {x}");
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
 }
