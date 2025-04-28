57c6d5273fe6690c5248fe564dfb63e843bd98a3
jamestheengineer <jamestheengineer@gmail.com>
2025-04-28 19:45:01 -0400

    the right way to while

--- rust2024/loops/src/main.rs
+++ rust2024/loops/src/main.rs
@@ -28,4 +28,19 @@ fn main() {
     }
 
     println!("LIFTOFF!!!");
+
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < 5 {
+        println!("the value is: {}", a[index]);
+
+        index += 1;
+    }
+
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a {
+        println!("the value is: {element}");
+    }
 }
