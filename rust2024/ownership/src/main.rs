2c643a8fb2d51f18f6ca2d54b920b89a9f4a200d
jamestheengineer <jamestheengineer@gmail.com>
2025-05-10 07:28:17 -0400

    slices

--- rust2024/ownership/src/main.rs
+++ rust2024/ownership/src/main.rs
@@ -1,12 +1,24 @@
 fn main() {
-    let mut s = String::from("hello");
+    let s = String::from("hello");
 
-    let r1 = &s; // no problem
-    let r2 = &s; // no problem
-    println!("{r1} and {r2}");
-    // Variables r1 and r2 will not be used after this point.
+    let slice = &s[0..2];
+    println!("{}", slice);
+    let slice = &s[..2];
+    println!("{}", slice);
 
-    let r3 = &mut s; // no problem
-    println!("{r3}");
+    let len = s.len();
+    println!("{}", len);
 
+    let slice = &s[3..len];
+    println!("{}", slice);
+    let slice = &s[3..];
+    println!("{}", slice);
+
+    let len = s.len();
+
+    let slice = &s[0..len];
+    println!("{}", slice);
+    let slice = &s[..];
+
+    println!("{}", slice);
 }
