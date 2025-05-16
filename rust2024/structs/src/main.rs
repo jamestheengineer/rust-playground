b3f1877c6e93de56d27e9394b3651e97c5ffb285
jamestheengineer <jamestheengineer@gmail.com>
2025-05-16 17:20:55 -0400

    tuple structs and unit structs

--- rust2024/structs/src/main.rs
+++ rust2024/structs/src/main.rs
@@ -12,7 +12,16 @@ fn main() {
         email: String::from("another@example.com"),
         ..user1
     };
+
+    // tuple structs
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+
+    let subject = AlwaysEqual;
 }
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
+struct AlwaysEqual;
 
 struct User {
     active: bool,
