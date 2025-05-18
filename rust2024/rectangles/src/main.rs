b817190919494eed638e469826c7e75145d25187
jamestheengineer <jamestheengineer@gmail.com>
2025-05-18 07:43:28 -0400

    tuples refactor

--- rust2024/rectangles/src/main.rs
+++ rust2024/rectangles/src/main.rs
@@ -6,8 +6,20 @@ fn main() {
         "The area of the rectangle is {} square pixels.",
         area(width1, height1)
     );
+
+    // refactor with tuples
+    let rect1 = (30, 50);
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area2(rect1)
+    );
 }
 
 fn area(width: u32, height: u32) -> u32 {
     width * height
+}
+
+fn area2(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
 }
