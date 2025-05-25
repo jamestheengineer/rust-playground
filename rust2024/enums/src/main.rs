244caa523a09e1aa7a868bc89c7bf9d3ef90d372
jamestheengineer <jamestheengineer@gmail.com>
2025-05-25 08:34:19 -0400

    better enums

--- rust2024/enums/src/main.rs
+++ rust2024/enums/src/main.rs
@@ -4,10 +4,28 @@ fn main() {
 
     println!("four is {four:#?}");
     println!("six is {six:#?}");
+
+    let home = IpAddrBetter::V4(String::from("127.0.0.1"));
+
+    let loopback = IpAddrBetter::V6(String::from("::1"));
+
+    println!("home is {home:#?}");
+    println!("loopback is {loopback:#?}");
 }
 
 #[derive(Debug)]
 enum IpAddrKind {
     V4,
     V6,
+}
+
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
+#[derive(Debug)]
+enum IpAddrBetter {
+    V4(String),
+    V6(String),
 }
