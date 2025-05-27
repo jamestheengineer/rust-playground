8b68a1c0f8f1d9ca0e2c911c34033d07cedf243e
jamestheengineer <jamestheengineer@gmail.com>
2025-05-27 17:48:45 -0400

    options

--- rust2024/enums/src/main.rs
+++ rust2024/enums/src/main.rs
@@ -14,6 +14,11 @@ fn main() {
 
     let m = Message::Write(String::from("hello"));
     m.call();
+
+    let some_number = Some(5);
+    let some_char = Some('e');
+
+    let absent_number: Option<i32> = None;
 }
 
 #[derive(Debug)]
