d3726813691fa8796612296b0ff4bb9bda5c97c3
jamestheengineer <jamestheengineer@gmail.com>
2025-06-03 17:44:37 -0400

    if let and default case

--- rust2024/enums/src/main.rs
+++ rust2024/enums/src/main.rs
@@ -52,6 +52,14 @@ fn main() {
     if let Some(max) = config_max {
         println!("The maximum is configured to be {max}");
     }
+
+    let mut count = 0;
+    let coin = Coin::Quarter(UsState::North_Carolina);
+    if let Coin::Quarter(state) = coin {
+        println!("State quarter from {state:?}!");
+    } else {
+        count += 1;
+    }
 }
 
 fn add_fancy_hat() {}
