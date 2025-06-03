d1039afe4697aa0704544e6a6a626ab87119d08e
jamestheengineer <jamestheengineer@gmail.com>
2025-06-02 21:58:15 -0400

    if let

--- rust2024/enums/src/main.rs
+++ rust2024/enums/src/main.rs
@@ -41,6 +41,17 @@ fn main() {
         7 => remove_fancy_hat(),
         _ => (),
     }
+
+    let config_max = Some(3u8);
+    match config_max {
+        Some(max) => println!("The maximum is configured to be {max}"),
+        _ => (),
+    }
+
+    let config_max = Some(3u8);
+    if let Some(max) = config_max {
+        println!("The maximum is configured to be {max}");
+    }
 }
 
 fn add_fancy_hat() {}
