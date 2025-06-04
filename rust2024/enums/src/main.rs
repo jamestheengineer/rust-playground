a19cfb1813ca8e6133fa9e8e732ada1c4d5a5e1f
jamestheengineer <jamestheengineer@gmail.com>
2025-06-04 19:59:26 -0400

    let else

--- rust2024/enums/src/main.rs
+++ rust2024/enums/src/main.rs
@@ -66,6 +66,18 @@ fn add_fancy_hat() {}
 fn remove_fancy_hat() {}
 fn move_player(num_spaces: u8) {}
 
+fn describe_state_quarter(coin: Coin) -> Option<String> {
+    let Coin::Quarter(state) = coin else {
+        return None;
+    };
+
+    if state.existed_in(1900) {
+        Some(format!("{state:?} is pretty old, for America!"))
+    } else {
+        Some(format!("{state:?} is relatively new."))
+    }
+}
+
 #[derive(Debug)]
 enum IpAddrKind {
     V4,
@@ -121,6 +133,17 @@ enum Coin {
     Nickel,
     Dime,
     Quarter(UsState),
+}
+
+impl UsState {
+    fn existed_in(&self, year: u16) -> bool {
+        match self {
+            UsState::Alabama => year >= 1819,
+            UsState::Alaska => year >= 1959,
+            UsState::North_Carolina => year >= 2004,
+            // -- snip --
+        }
+    }
 }
 
 fn plus_one(x: Option<i32>) -> Option<i32> {
