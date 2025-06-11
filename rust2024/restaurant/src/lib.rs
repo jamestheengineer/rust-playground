3d00f8db2e8b1d6b976afb1f706afbfae216bfe9
jamestheengineer <jamestheengineer@gmail.com>
2025-06-11 18:27:52 -0400

    but enums go all the way down

--- rust2024/restaurant/src/lib.rs
+++ rust2024/restaurant/src/lib.rs
@@ -30,11 +30,11 @@ pub fn eat_at_restaurant() {
     // The next line won't compile if we uncomment it; we're not allowed
     // to see or modify the seasonal fruit that comes with the meal.
     // meal.seasonal_fruit = String::from("blueberries");
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
 }
 
 fn deliver_order() {}
-
-
 
 mod back_of_house {
     fn fix_incorrect_order() {
@@ -47,6 +47,11 @@ mod back_of_house {
     pub struct Breakfast {
         pub toast: String,
         seasonal_fruit: String,
+    }
+
+    pub enum Appetizer {
+        Soup,
+        Salad,
     }
 
     impl Breakfast {
