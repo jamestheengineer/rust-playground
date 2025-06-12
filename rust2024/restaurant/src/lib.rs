5d9dda69068410df3df92e9ee639571210f785e6
jamestheengineer <jamestheengineer@gmail.com>
2025-06-12 17:42:18 -0400

    use

--- rust2024/restaurant/src/lib.rs
+++ rust2024/restaurant/src/lib.rs
@@ -13,10 +13,12 @@ mod front_of_house {
         fn take_payment() {}
     }
 }
+use crate::front_of_house::hosting;
 
 pub fn eat_at_restaurant() {
     // Absolute path
-    crate::front_of_house::hosting::add_to_waitlist();
+    //crate::front_of_house::hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
 
     // Relative path
     front_of_house::hosting::add_to_waitlist();
