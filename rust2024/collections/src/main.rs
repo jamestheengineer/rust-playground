1105aab5cbbe603aeb432dc2c46eb86121587724
jamestheengineer <jamestheengineer@gmail.com>
2025-06-18 17:42:58 -0400

    using collections

--- rust2024/collections/src/main.rs
+++ rust2024/collections/src/main.rs
@@ -9,4 +9,15 @@ fn main() {
     v.push(6);
     v.push(7);
     v.push(8);
+
+    let v = vec![1, 2, 3, 4, 5];
+
+    let third: &i32 = &v[2];
+    println!("The third element is {third}");
+
+    let third: Option<&i32> = v.get(2);
+    match third {
+        Some(third) => println!("The third element is {third}"),
+        None => println!("There is no third element."),
+    }
 }
