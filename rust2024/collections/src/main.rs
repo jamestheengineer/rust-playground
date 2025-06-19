2daacb1a4e2726abec33921a013c43372a168a83
jamestheengineer <jamestheengineer@gmail.com>
2025-06-19 18:29:39 -0400

    iterate

--- rust2024/collections/src/main.rs
+++ rust2024/collections/src/main.rs
@@ -20,4 +20,14 @@ fn main() {
         Some(third) => println!("The third element is {third}"),
         None => println!("There is no third element."),
     }
+
+    let v = vec![100, 32, 57];
+    for i in &v {
+        println!("{i}");
+    }
+
+    let mut v = vec![100, 32, 57];
+    for i in &mut v {
+        *i += 50;
+    }
 }
