4170cc7b7cce3571876347ce3a99ece3fcc50be7
jamestheengineer <jamestheengineer@gmail.com>
2025-06-20 17:06:57 -0400

    how to add different things to a vec through an enum

--- rust2024/collections/src/main.rs
+++ rust2024/collections/src/main.rs
@@ -30,4 +30,16 @@ fn main() {
     for i in &mut v {
         *i += 50;
     }
+
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
+    }
+
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
 }
