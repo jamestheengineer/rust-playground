ba7e6a69feb5b26470e539a8324cbc0b3d75f8ca
jamestheengineer <jamestheengineer@gmail.com>
2025-06-23 18:07:34 -0400

    hash maps

--- rust2024/collections/src/main.rs
+++ rust2024/collections/src/main.rs
@@ -87,4 +87,18 @@ fn main() {
     for b in "Зд".bytes() {
         println!("{b}");
     }
+
+    use std::collections::HashMap;
+
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name).copied().unwrap_or(0);
+
+    for (key, value) in &scores {
+        println!("{key}: {value}");
+    }
 }
