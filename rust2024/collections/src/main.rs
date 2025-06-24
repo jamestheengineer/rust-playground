c04f255f9d125159bc39745513c8f8931e077d67
jamestheengineer <jamestheengineer@gmail.com>
2025-06-24 17:50:05 -0400

    move rules apply to hashmaps

--- rust2024/collections/src/main.rs
+++ rust2024/collections/src/main.rs
@@ -101,4 +101,12 @@ fn main() {
     for (key, value) in &scores {
         println!("{key}: {value}");
     }
+
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+    // field_name and field_value are invalid at this point, try using them and
+    // see what compiler error you get!
 }
