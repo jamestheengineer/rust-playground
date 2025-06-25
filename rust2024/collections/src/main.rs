514d5fdd9b86a07b6ba3df55e6ee8952628a2c8f
jamestheengineer <jamestheengineer@gmail.com>
2025-06-25 18:20:57 -0400

    hash rewrites

--- rust2024/collections/src/main.rs
+++ rust2024/collections/src/main.rs
@@ -109,4 +109,12 @@ fn main() {
     map.insert(field_name, field_value);
     // field_name and field_value are invalid at this point, try using them and
     // see what compiler error you get!
+
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+
+    scores.entry(String::from("Yellow")).or_insert(50);
+    scores.entry(String::from("Blue")).or_insert(50);
+
+    println!("{scores:?}");
 }
