ca2d1ffd34548cff4e608ba5fd817789368433b3
jamestheengineer <jamestheengineer@gmail.com>
2025-06-30 16:59:41 -0400

    cleaner way

--- rust2024/error_handling/src/main.rs
+++ rust2024/error_handling/src/main.rs
@@ -16,4 +16,14 @@ fn main() {
             }
         },
     };
+
+    let greeting_file = File::open("hello2.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello2.txt").unwrap_or_else(|error| {
+                panic!("Problem creating the file: {error:?}");
+            })
+        } else {
+            panic!("Problem opening the file: {error:?}");
+        }
+    });
 }
