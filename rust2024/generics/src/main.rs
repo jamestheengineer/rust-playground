a9f107e825e4187c9bedefe6a58fea4b11fe6b3f
jamestheengineer <jamestheengineer@gmail.com>
2025-07-12 08:33:28 -0400

    mixup

--- rust2024/generics/src/main.rs
+++ rust2024/generics/src/main.rs
@@ -1,45 +1,22 @@
-fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
-    let mut largest = &list[0];
+struct Point<X1, Y1> {
+    x: X1,
+    y: Y1,
+}
 
-    for item in list {
-        if item > largest {
-            largest = item;
+impl<X1, Y1> Point<X1, Y1> {
+    fn mixup<X2, Y2>(self, other: Point<X2, Y2>) -> Point<X1, Y2> {
+        Point {
+            x: self.x,
+            y: other.y,
         }
     }
-
-    largest
 }
-
-struct Point<T> {
-    x: T,
-    y: T,
-}
-
-impl<T> Point<T> {
-    fn x(&self) -> &T {
-        &self.x
-    }
-}
-
-impl Point<f32> {
-    fn distance_from_origin(&self) -> f32 {
-        (self.x.powi(2) + self.y.powi(2)).sqrt()
-    }
-}
-
 
 fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
+    let p1 = Point { x: 5, y: 10.4 };
+    let p2 = Point { x: "Hello", y: 'c' };
 
-    let result = largest(&number_list);
-    println!("The largest number is {result}");
+    let p3 = p1.mixup(p2);
 
-    let char_list = vec!['y', 'm', 'a', 'q'];
-
-    let result = largest(&char_list);
-    println!("The largest char is {result}");
-
-    let p = Point { x: 5, y: 10 };
-
-    println!("p.x = {}", p.x());
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
 }
