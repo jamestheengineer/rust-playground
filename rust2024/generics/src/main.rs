a4c0d34fb7cf2f72b5a62486a594f946b93dfd84
jamestheengineer <jamestheengineer@gmail.com>
2025-07-17 18:16:18 -0400

    lifetimes

--- rust2024/generics/src/main.rs
+++ rust2024/generics/src/main.rs
@@ -1,67 +1,11 @@
-pub trait Summary {
-    fn summarize_author(&self) -> String;
-
-    fn summarize(&self) -> String {
-        format!("(Read more from {}...)", self.summarize_author())
-    }
-}
-
-pub struct NewsArticle {
-    pub headline: String,
-    pub location: String,
-    pub author: String,
-    pub content: String,
-}
-
-pub struct SocialPost {
-    pub username: String,
-    pub content: String,
-    pub reply: bool,
-    pub repost: bool,
-}
-
-
-impl Summary for SocialPost {
-    fn summarize_author(&self) -> String {
-        format!("@{}", self.username)
-    }
-}
-
-pub fn notify(item: &impl Summary) {
-    println!("Breaking news! {}", item.summarize());
-}
-
-pub fn notify2<T: Summary>(item: &T) {
-    println!("Breaking news! {}", item.summarize());
-}
-
 fn main() {
-    let post = SocialPost {
-        username: String::from("horse_ebooks"),
-        content: String::from("of course, as you probably already know, people"),
-        reply: false,
-        repost: false,
-    };
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
 
-    println!("1 new post: {}", post.summarize());
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {result}");
+}
 
-    let article = NewsArticle {
-        headline: String::from("Penguins win the Stanley Cup Championship!"),
-        location: String::from("Pittsburgh, PA, USA"),
-        author: String::from("Iceburgh"),
-        content: String::from(
-            "The Pittsburgh Penguins once again are the best \
-             hockey team in the NHL.",
-        ),
-    };
-
-
-    let post = SocialPost {
-        username: String::from("horse_ebooks"),
-        content: String::from("of course, as you probably already know, people"),
-        reply: false,
-        repost: false,
-    };
-
-    println!("1 new post: {}", post.summarize());
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() { x } else { y }
 }
