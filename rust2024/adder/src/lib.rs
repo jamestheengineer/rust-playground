63791f54deb1f27d330868aed7823d20a11db7cd
jamestheengineer <jamestheengineer@gmail.com>
2025-07-24 17:25:50 -0400

    result instead of panic

--- rust2024/adder/src/lib.rs
+++ rust2024/adder/src/lib.rs
@@ -24,12 +24,18 @@ pub struct Guess {
 
 impl Guess {
     pub fn new(value: i32) -> Guess {
-        if value < 1 || value > 100 {
-            panic!("Guess value must be between 1 and 100, got {value}.");
+        if value < 1 {
+            panic!("Guess value must be greater than or equal to 1, got {value}.");
+        } else if value > 100 {
+            panic!("Guess value must be less than or equal to 100, got {value}.");
         }
 
         Guess { value }
     }
+}
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
 }
 
 #[cfg(test)]
@@ -37,7 +43,18 @@ mod tests {
     use super::*;
 
     #[test]
-    #[should_panic]
+    fn it_works() -> Result<(), String> {
+        let result = add(2, 2);
+
+        if result == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
+    }
+
+    #[test]
+    #[should_panic(expected = "less than or equal to 100")]
     fn greater_than_100() {
         Guess::new(200);
     }
