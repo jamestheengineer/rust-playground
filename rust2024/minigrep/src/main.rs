3511fae7e6a51a563531b1a87d4904a816bf3ef4
jamestheengineer <jamestheengineer@gmail.com>
2025-08-10 16:24:01 -0600

    break out library

--- rust2024/minigrep/src/main.rs
+++ rust2024/minigrep/src/main.rs
@@ -2,6 +2,7 @@ use std::env;
 use std::error::Error;
 use std::fs;
 use std::process;
+use minigrep::search;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -10,9 +11,6 @@ fn main() {
         println!("Problem parsing arguments: {err}");
         process::exit(1);
     });
-
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
 
     if let Err(e) = run(config) {
         println!("Application error: {e}");
@@ -23,7 +21,9 @@ fn main() {
 fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    println!("With text:\n{contents}");
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
 
     Ok(())
 }
