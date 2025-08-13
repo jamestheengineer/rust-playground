cfa3e8c4493a56fc4a607a1fad8ee5c24b6eb3d2
jamestheengineer <jamestheengineer@gmail.com>
2025-08-12 21:01:19 -0600

    command line testing

---
+++ rust2024/minigrep/src/lib.rs
@@ -0,0 +1,33 @@
+use std::io::empty;
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    if results.is_empty() {
+      results.push("No results found");
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
