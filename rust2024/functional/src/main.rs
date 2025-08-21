12f1e895089ac40d4388873408eacf6bbc82575b
jamestheengineer <jamestheengineer@gmail.com>
2025-08-20 20:47:58 -0400

    iterators

--- rust2024/functional/src/main.rs
+++ rust2024/functional/src/main.rs
@@ -73,4 +73,12 @@ fn main() {
     list.sort_by_key(|r| r.height);
     list.sort_by_key(|r| r.width);
     println!("{list:#?}");
+
+    let v1 = vec![1, 2, 3];
+
+    let v1_iter = v1.iter();
+
+    for val in v1_iter {
+        println!("Got: {val}");
+    }
 }
