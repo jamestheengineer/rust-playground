ded05868f327fe4322f81061971d9919b0120caa
jamestheengineer <jamestheengineer@gmail.com>
2025-08-22 19:39:27 -0400

    iter usage

--- rust2024/functional/src/main.rs
+++ rust2024/functional/src/main.rs
@@ -98,4 +98,10 @@ fn main() {
     let total: i32 = v1_iter.sum();
 
     assert_eq!(total, 6);
+
+    let v1: Vec<i32> = vec![1, 2, 3];
+
+    let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
+
+    assert_eq!(v2, vec![2, 3, 4]);
 }
