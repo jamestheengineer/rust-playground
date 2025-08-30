eb3bb0eb949152c5f9bd03ef9f682b82aef58476
jamestheengineer <jamestheengineer@gmail.com>
2025-08-30 08:15:18 -0400

    re-exporting for a better api

--- rust2024/minigrep/src/lib.rs
+++ rust2024/minigrep/src/lib.rs
@@ -2,8 +2,34 @@
 //!
 //! `my_crate` is a collection of utilities to make performing certain
 //! calculations more convenient.
+pub use self::kinds::PrimaryColor;
+pub use self::kinds::SecondaryColor;
+pub use self::utils::mix;
+pub mod kinds {
+    /// The primary colors according to the RYB color model.
+    pub enum PrimaryColor {
+        Red,
+        Yellow,
+        Blue,
+    }
 
+    /// The secondary colors according to the RYB color model.
+    pub enum SecondaryColor {
+        Orange,
+        Green,
+        Purple,
+    }
+}
 
+pub mod utils {
+    use crate::kinds::*;
+
+    /// Combines two primary colors in equal amounts to create
+    /// a secondary color.
+    pub fn mix(c1: PrimaryColor, c2: PrimaryColor) -> SecondaryColor {
+        // --snip--
+    }
+}
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     contents
