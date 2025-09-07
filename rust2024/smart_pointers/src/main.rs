64231a5cd31468f94783850c61ccb3f84460633c
jamestheengineer <jamestheengineer@gmail.com>
2025-09-07 09:16:37 -0400

    references

--- rust2024/smart_pointers/src/main.rs
+++ rust2024/smart_pointers/src/main.rs
@@ -6,5 +6,10 @@ enum List {
 use crate::List::{Cons, Nil};
 
 fn main() {
-    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+
+    let x = 5;
+    let y = &x;
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
 }
