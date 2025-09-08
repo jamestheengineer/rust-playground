2102ee8bee531d986991e44239979584bb0ef71b
jamestheengineer <jamestheengineer@gmail.com>
2025-09-08 18:14:46 -0400

    refs

--- rust2024/smart_pointers/src/main.rs
+++ rust2024/smart_pointers/src/main.rs
@@ -1,14 +1,13 @@
-enum List {
-    Cons(i32, Box<List>),
-    Nil,
-}
-
-use crate::List::{Cons, Nil};
-
 fn main() {
 
     let x = 5;
     let y = &x;
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    let x = 5;
+    let y = Box::new(x);
 
     assert_eq!(5, x);
     assert_eq!(5, *y);
