2262a11ae8434278b0b9c170cb3d8716d0287cc8
jamestheengineer <jamestheengineer@gmail.com>
2025-09-13 09:44:33 -0400

    ref counted pointers

--- rust2024/smart_pointers/src/main.rs
+++ rust2024/smart_pointers/src/main.rs
@@ -26,6 +26,13 @@ impl Drop for CustomSmartPointer {
     }
 }
 
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
+}
+use crate::List::{Cons, Nil};
+use std::rc::Rc;
+
 fn main() {
     let c = CustomSmartPointer {
         data: String::from("my stuff"),
@@ -50,6 +57,10 @@ fn main() {
     println!("CustomSmartPointer created.");
     drop(c);
     println!("CustomSmartPointer dropped before the end of main.");
+
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    let b = Cons(3, Rc::clone(&a));
+    let c = Cons(4, Rc::clone(&a));
 }
 
 fn hello(name: &str) {
