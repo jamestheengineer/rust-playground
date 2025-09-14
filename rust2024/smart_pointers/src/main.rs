6332804b2e1050b4264b9844f618c77c476549ba
jamestheengineer <jamestheengineer@gmail.com>
2025-09-14 09:53:18 -0400

    more ref counting

--- rust2024/smart_pointers/src/main.rs
+++ rust2024/smart_pointers/src/main.rs
@@ -34,35 +34,13 @@ use crate::List::{Cons, Nil};
 use std::rc::Rc;
 
 fn main() {
-    let c = CustomSmartPointer {
-        data: String::from("my stuff"),
-    };
-    let d = CustomSmartPointer {
-        data: String::from("other stuff"),
-    };
-    println!("CustomSmartPointers created.");
-
-    let x = 5;
-    let y = MyBox::new(x);
-
-    assert_eq!(5, x);
-    assert_eq!(5, *y);
-
-    let m = MyBox::new(String::from("Rust"));
-    hello(&m);
-
-    let c = CustomSmartPointer {
-        data: String::from("some data"),
-    };
-    println!("CustomSmartPointer created.");
-    drop(c);
-    println!("CustomSmartPointer dropped before the end of main.");
-
     let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
     let b = Cons(3, Rc::clone(&a));
-    let c = Cons(4, Rc::clone(&a));
-}
-
-fn hello(name: &str) {
-    println!("Hello, {name}!");
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 }
