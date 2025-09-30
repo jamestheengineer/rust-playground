ac7a3203fa99b1a1708ff4d057f165deee042480
jamestheengineer <jamestheengineer@gmail.com>
2025-09-30 18:34:29 -0400

    await

--- rust2024/hello-async/src/main.rs
+++ rust2024/hello-async/src/main.rs
@@ -1,3 +1,23 @@
+use std::future::Future;
+use trpl::Html;
+
+fn page_title(url: &str) -> impl Future<Output = Option<String>> {
+    async move {
+        let text = trpl::get(url).await.text().await;
+        Html::parse(&text)
+            .select_first("title")
+            .map(|title| title.inner_html())
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+
+    trpl::run(async {
+        let url = &args[1];
+        match page_title(url).await {
+            Some(title) => println!("The title for {url} was {title}"),
+            None => println!("{url} had no title"),
+        }
+    })
 }
