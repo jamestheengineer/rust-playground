1d955c2843d1ec3f4f744bea04bc8ee8e4832579
jamestheengineer <jamestheengineer@gmail.com>
2025-10-03 20:05:27 -0400

    future join

--- rust2024/hello-async/src/main.rs
+++ rust2024/hello-async/src/main.rs
@@ -2,18 +2,20 @@ use std::time::Duration;
 
 fn main() {
     trpl::run(async {
-        let handle = trpl::spawn_task(async {
+        let fut1 = async {
             for i in 1..10 {
                 println!("hi number {i} from the first task!");
                 trpl::sleep(Duration::from_millis(500)).await;
             }
-        });
+        };
 
-        for i in 1..5 {
-            println!("hi number {i} from the second task!");
-            trpl::sleep(Duration::from_millis(500)).await;
-        }
+        let fut2 = async {
+            for i in 1..5 {
+                println!("hi number {i} from the second task!");
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
+        };
 
-        handle.await.unwrap();
+        trpl::join(fut1, fut2).await;
     });
 }
