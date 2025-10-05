55e6e73b251585de9c316aa26bb9f5cc148dd787
jamestheengineer <jamestheengineer@gmail.com>
2025-10-05 11:06:35 -0400

    half solution

--- rust2024/hello-async/src/main.rs
+++ rust2024/hello-async/src/main.rs
@@ -2,28 +2,22 @@ use std::time::Duration;
 
 fn main() {
     trpl::run(async {
-        let fut1 = async {
-            for i in 1..10 {
-                println!("hi number {i} from the first task!");
-                trpl::sleep(Duration::from_millis(500)).await;
-            }
-        };
-
-        let fut2 = async {
-            for i in 1..5 {
-                println!("hi number {i} from the second task!");
-                trpl::sleep(Duration::from_millis(500)).await;
-            }
-        };
-
-        trpl::join(fut1, fut2).await;
-
         let (tx, mut rx) = trpl::channel();
 
-        let val = String::from("transport!");
-        tx.send(val).unwrap();
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("future"),
+        ];
 
-        let received = rx.recv().await.unwrap();
-        println!("received '{received}'");
+        for val in vals {
+            tx.send(val).unwrap();
+            trpl::sleep(Duration::from_millis(500)).await;
+        }
+
+        while let Some(value) = rx.recv().await {
+            println!("received '{value}'");
+        }
     });
 }
