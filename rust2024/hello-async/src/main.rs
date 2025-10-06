ea77158361b002fdfb853f93b866592e69e8a182
jamestheengineer <jamestheengineer@gmail.com>
2025-10-06 18:05:16 -0400

    now it exits because of move

--- rust2024/hello-async/src/main.rs
+++ rust2024/hello-async/src/main.rs
@@ -4,20 +4,26 @@ fn main() {
     trpl::run(async {
         let (tx, mut rx) = trpl::channel();
 
-        let vals = vec![
-            String::from("hi"),
-            String::from("from"),
-            String::from("the"),
-            String::from("future"),
-        ];
+        let tx_fut = async move {
+            let vals = vec![
+                String::from("hi"),
+                String::from("from"),
+                String::from("the"),
+                String::from("future"),
+            ];
 
-        for val in vals {
-            tx.send(val).unwrap();
-            trpl::sleep(Duration::from_millis(500)).await;
-        }
+            for val in vals {
+                tx.send(val).unwrap();
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
+        };
 
-        while let Some(value) = rx.recv().await {
-            println!("received '{value}'");
-        }
+        let rx_fut = async {
+            while let Some(value) = rx.recv().await {
+                println!("received '{value}'");
+            }
+        };
+
+        trpl::join(tx_fut, rx_fut).await;
     });
 }
