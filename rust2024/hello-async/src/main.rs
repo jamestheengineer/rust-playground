b5488bba4646a859dbe08787bbd5670cf57a199a
jamestheengineer <jamestheengineer@gmail.com>
2025-10-07 17:31:46 -0400

    more async

--- rust2024/hello-async/src/main.rs
+++ rust2024/hello-async/src/main.rs
@@ -4,7 +4,8 @@ fn main() {
     trpl::run(async {
         let (tx, mut rx) = trpl::channel();
 
-        let tx_fut = async move {
+        let tx1 = tx.clone();
+        let tx1_fut = async move {
             let vals = vec![
                 String::from("hi"),
                 String::from("from"),
@@ -13,7 +14,7 @@ fn main() {
             ];
 
             for val in vals {
-                tx.send(val).unwrap();
+                tx1.send(val).unwrap();
                 trpl::sleep(Duration::from_millis(500)).await;
             }
         };
@@ -24,6 +25,20 @@ fn main() {
             }
         };
 
-        trpl::join(tx_fut, rx_fut).await;
+        let tx_fut = async move {
+            let vals = vec![
+                String::from("more"),
+                String::from("messages"),
+                String::from("for"),
+                String::from("you"),
+            ];
+
+            for val in vals {
+                tx.send(val).unwrap();
+                trpl::sleep(Duration::from_millis(1500)).await;
+            }
+        };
+
+        trpl::join3(tx1_fut, tx_fut, rx_fut).await;
     });
 }
