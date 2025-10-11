e919ae144225fbfa9c2f091c376277dc2ddd3762
jamestheengineer <jamestheengineer@gmail.com>
2025-10-11 08:13:08 -0400

    not what you want

--- rust2024/hello-async/src/main.rs
+++ rust2024/hello-async/src/main.rs
@@ -1,68 +1,31 @@
-use std::pin::Pin;
+use std::thread;
 use std::time::Duration;
-
 fn main() {
     trpl::run(async {
-        let (tx, mut rx) = trpl::channel();
-
-        let tx1 = tx.clone();
-        let tx1_fut = async move {
-            let vals = vec![
-                String::from("hi"),
-                String::from("from"),
-                String::from("the"),
-                String::from("future"),
-            ];
-
-            for val in vals {
-                tx1.send(val).unwrap();
-                trpl::sleep(Duration::from_millis(500)).await;
-            }
-        };
-
-        let rx_fut = async {
-            while let Some(value) = rx.recv().await {
-                println!("received '{value}'");
-            }
-        };
-
-        let tx_fut = async move {
-            let vals = vec![
-                String::from("more"),
-                String::from("messages"),
-                String::from("for"),
-                String::from("you"),
-            ];
-
-            for val in vals {
-                tx.send(val).unwrap();
-                trpl::sleep(Duration::from_millis(1500)).await;
-            }
-        };
-
-        let futures: Vec<Pin<Box<dyn Future<Output = ()>>>> =
-            vec![Box::pin(tx1_fut), Box::pin(rx_fut), Box::pin(tx_fut)];
-        trpl::join_all(futures).await;
-
-        let a = async { 1u32 };
-        let b = async { "Hello!" };
-        let c = async { true };
-
-        let (a_result, b_result, c_result) = trpl::join!(a, b, c);
-        println!("{a_result}, {b_result}, {c_result}");
-
-        let slow = async {
-            println!("'slow' started.");
-            trpl::sleep(Duration::from_millis(100)).await;
-            println!("'slow' finished.");
-        };
-
-        let fast = async {
-            println!("'fast' started.");
+        let a = async {
+            println!("'a' started.");
+            slow("a", 30);
+            slow("a", 10);
+            slow("a", 20);
             trpl::sleep(Duration::from_millis(50)).await;
-            println!("'fast' finished.");
+            println!("'a' finished.");
         };
 
-        trpl::race(slow, fast).await;
+        let b = async {
+            println!("'b' started.");
+            slow("b", 75);
+            slow("b", 10);
+            slow("b", 15);
+            slow("b", 350);
+            trpl::sleep(Duration::from_millis(50)).await;
+            println!("'b' finished.");
+        };
+
+        trpl::race(a, b).await;
     });
+}
+
+fn slow(name: &str, ms: u64) {
+    thread::sleep(Duration::from_millis(ms));
+    println!("'{name}' ran for {ms}ms");
 }
