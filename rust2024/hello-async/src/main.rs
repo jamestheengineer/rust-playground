d73c313b0c58557f7cce709e31651db161c7f177
jamestheengineer <jamestheengineer@gmail.com>
2025-10-15 19:00:31 -0400

    streams

--- rust2024/hello-async/src/main.rs
+++ rust2024/hello-async/src/main.rs
@@ -1,26 +1,13 @@
-use std::time::Duration;
-
-use ::trpl::Either;
+use trpl::StreamExt;
 
 fn main() {
     trpl::run(async {
-        let slow = async {
-            trpl::sleep(Duration::from_secs(5)).await;
-            "Finally finished"
-        };
+        let values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        let iter = values.iter().map(|n| n * 2);
+        let mut stream = trpl::stream_from_iter(iter);
 
-        match timeout(slow, Duration::from_secs(6)).await {
-            Ok(message) => println!("Succeeded with '{message}'"),
-            Err(duration) => {
-                println!("Failed after {} seconds", duration.as_secs())
-            }
+        while let Some(value) = stream.next().await {
+            println!("The value was: {value}");
         }
     });
-}
-
-async fn timeout<F: Future>(future_to_try: F, max_time: Duration) -> Result<F::Output, Duration> {
-    match trpl::race(future_to_try, trpl::sleep(max_time)).await {
-        Either::Left(output) => Ok(output),
-        Either::Right(_) => Err(max_time),
-    }
 }
