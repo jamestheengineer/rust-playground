c7e66f94ee928fbb1b96304d2d1f4c512641d599
jamestheengineer <jamestheengineer@gmail.com>
2025-10-16 18:18:07 -0400

    stream messages

--- rust2024/hello-async/src/main.rs
+++ rust2024/hello-async/src/main.rs
@@ -1,13 +1,22 @@
-use trpl::StreamExt;
+use trpl::{ReceiverStream, Stream, StreamExt};
 
 fn main() {
     trpl::run(async {
-        let values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-        let iter = values.iter().map(|n| n * 2);
-        let mut stream = trpl::stream_from_iter(iter);
+        let mut messages = get_messages();
 
-        while let Some(value) = stream.next().await {
-            println!("The value was: {value}");
+        while let Some(message) = messages.next().await {
+            println!("{message}");
         }
     });
+}
+
+fn get_messages() -> impl Stream<Item = String> {
+    let (tx, rx) = trpl::channel();
+
+    let messages = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
+    for message in messages {
+        tx.send(format!("Message: '{message}'")).unwrap();
+    }
+
+    ReceiverStream::new(rx)
 }
