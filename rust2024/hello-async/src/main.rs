3efbc25c045b5f8b56a5322d4fcf7d0be378599b
jamestheengineer <jamestheengineer@gmail.com>
2025-10-19 07:32:03 -0400

    throttling is cool

--- rust2024/hello-async/src/main.rs
+++ rust2024/hello-async/src/main.rs
@@ -6,8 +6,9 @@ fn main() {
         let messages = get_messages().timeout(Duration::from_millis(200));
         let intervals = get_intervals()
             .map(|count| format!("Interval: {count}"))
+            .throttle(Duration::from_millis(100))
             .timeout(Duration::from_secs(10));
-        let merged = messages.merge(intervals);
+        let merged = messages.merge(intervals).take(20);
         let mut stream = pin!(merged);
 
         while let Some(result) = stream.next().await {
