236995a7e17bab2c30225da6398a3badf6bcb2ef
jamestheengineer <jamestheengineer@gmail.com>
2025-10-21 18:39:34 -0400

    losing me

--- rust2024/hello-async/src/main.rs
+++ rust2024/hello-async/src/main.rs
@@ -1,4 +1,5 @@
 use std::pin::pin;
+use std::thread;
 use std::time::Duration;
 use trpl::{ReceiverStream, Stream, StreamExt};
 fn main() {
@@ -43,10 +44,12 @@ fn get_messages() -> impl Stream<Item = String> {
 fn get_intervals() -> impl Stream<Item = u32> {
     let (tx, rx) = trpl::channel();
 
-    trpl::spawn_task(async move {
+    // This is *not* `trpl::spawn` but `std::thread::spawn`!
+    thread::spawn(move || {
         let mut count = 0;
         loop {
-            trpl::sleep(Duration::from_millis(1)).await;
+            // Likewise, this is *not* `trpl::sleep` but `std::thread::sleep`!
+            thread::sleep(Duration::from_millis(1));
             count += 1;
 
             if let Err(send_error) = tx.send(count) {
