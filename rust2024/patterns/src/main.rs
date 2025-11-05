a38cd1dd44e7d331f0e382f1026393df35b3e855
jamestheengineer <jamestheengineer@gmail.com>
2025-11-05 18:13:52 -0500

    shawdow

--- rust2024/patterns/src/main.rs
+++ rust2024/patterns/src/main.rs
@@ -45,6 +45,17 @@ fn main() {
         3 => println!("three"),
         _ => println!("anything"),
     }
+
+    let x = Some(5);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(y) => println!("Matched, y = {y}"),
+        _ => println!("Default case, x = {x:?}"),
+    }
+
+    println!("at the end: x = {x:?}, y = {y}");
 }
 
 fn print_coordinates(&(x, y): &(i32, i32)) {
