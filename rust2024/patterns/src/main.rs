e97257108576203f25c2fd83da24fd9b43bd2f5e
jamestheengineer <jamestheengineer@gmail.com>
2025-11-07 17:12:43 -0500

    ranges

--- rust2024/patterns/src/main.rs
+++ rust2024/patterns/src/main.rs
@@ -64,6 +64,13 @@ fn main() {
         3 => println!("three"),
         _ => println!("anything"),
     }
+
+    let x = 5;
+
+    match x {
+        1..=5 => println!("one through five"),
+        _ => println!("something else"),
+    }
 }
 
 fn print_coordinates(&(x, y): &(i32, i32)) {
