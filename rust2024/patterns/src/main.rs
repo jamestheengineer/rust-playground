843b6fd1df8e1dfc341b532336cd4344ba505012
jamestheengineer <jamestheengineer@gmail.com>
2025-11-09 10:24:39 -0500

    destructure

--- rust2024/patterns/src/main.rs
+++ rust2024/patterns/src/main.rs
@@ -71,8 +71,27 @@ fn main() {
         1..=5 => println!("one through five"),
         _ => println!("something else"),
     }
+
+    let x = 'c';
+
+    match x {
+        'a'..='j' => println!("early ASCII letter"),
+        'k'..='z' => println!("late ASCII letter"),
+        _ => println!("something else"),
+    }
+
+    let p = Point { x: 0, y: 7 };
+
+    let Point { x: a, y: b } = p;
+    assert_eq!(0, a);
+    assert_eq!(7, b);
 }
 
 fn print_coordinates(&(x, y): &(i32, i32)) {
     println!("Current location: ({x}, {y})");
+}
+
+struct Point {
+    x: i32,
+    y: i32,
 }
