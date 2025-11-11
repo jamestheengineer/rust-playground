7c6918945ea3f998bc3b1322b675f2576394ffce
jamestheengineer <jamestheengineer@gmail.com>
2025-11-10 19:06:38 -0500

    more matching and destructuring

--- rust2024/patterns/src/main.rs
+++ rust2024/patterns/src/main.rs
@@ -85,6 +85,16 @@ fn main() {
     let Point { x: a, y: b } = p;
     assert_eq!(0, a);
     assert_eq!(7, b);
+
+    let p = Point { x: 0, y: 7 };
+
+    match p {
+        Point { x, y: 0 } => println!("On the x axis at {x}"),
+        Point { x: 0, y } => println!("On the y axis at {y}"),
+        Point { x, y } => {
+            println!("On neither axis: ({x}, {y})");
+        }
+    }
 }
 
 fn print_coordinates(&(x, y): &(i32, i32)) {
