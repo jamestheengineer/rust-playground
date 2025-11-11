eb760dc70d84eac2d80c7cfcfd2d43dbeab4934c
jamestheengineer <jamestheengineer@gmail.com>
2025-11-11 18:50:57 -0500

    destructure enums

--- rust2024/patterns/src/main.rs
+++ rust2024/patterns/src/main.rs
@@ -95,6 +95,23 @@ fn main() {
             println!("On neither axis: ({x}, {y})");
         }
     }
+
+    let msg = Message::ChangeColor(0, 160, 255);
+
+    match msg {
+        Message::Quit => {
+            println!("The Quit variant has no data to destructure.");
+        }
+        Message::Move { x, y } => {
+            println!("Move in the x direction {x} and in the y direction {y}");
+        }
+        Message::Write(text) => {
+            println!("Text message: {text}");
+        }
+        Message::ChangeColor(r, g, b) => {
+            println!("Change color to red {r}, green {g}, and blue {b}");
+        }
+    }
 }
 
 fn print_coordinates(&(x, y): &(i32, i32)) {
@@ -104,4 +121,11 @@ fn print_coordinates(&(x, y): &(i32, i32)) {
 struct Point {
     x: i32,
     y: i32,
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
 }
