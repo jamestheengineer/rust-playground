c271bc324316103643c0db9708f026eff9fbe10b
jamestheengineer <jamestheengineer@gmail.com>
2025-11-12 19:55:37 -0500

    destructure nested enums

--- rust2024/patterns/src/main.rs
+++ rust2024/patterns/src/main.rs
@@ -96,21 +96,16 @@ fn main() {
         }
     }
 
-    let msg = Message::ChangeColor(0, 160, 255);
+    let msg = Message::ChangeColor(Color::Hsv(0, 160, 255));
 
     match msg {
-        Message::Quit => {
-            println!("The Quit variant has no data to destructure.");
-        }
-        Message::Move { x, y } => {
-            println!("Move in the x direction {x} and in the y direction {y}");
-        }
-        Message::Write(text) => {
-            println!("Text message: {text}");
-        }
-        Message::ChangeColor(r, g, b) => {
+        Message::ChangeColor(Color::Rgb(r, g, b)) => {
             println!("Change color to red {r}, green {g}, and blue {b}");
         }
+        Message::ChangeColor(Color::Hsv(h, s, v)) => {
+            println!("Change color to hue {h}, saturation {s}, value {v}");
+        }
+        _ => (),
     }
 }
 
@@ -127,5 +122,11 @@ enum Message {
     Quit,
     Move { x: i32, y: i32 },
     Write(String),
-    ChangeColor(i32, i32, i32),
+    ChangeColor(Color),
 }
+
+enum Color {
+    Rgb(i32, i32, i32),
+    Hsv(i32, i32, i32),
+}
+
