a2c3eea1f1dd330c427b77c575f21cb63873d44c
jamestheengineer <jamestheengineer@gmail.com>
2025-11-28 21:47:38 -0500

    op overload

--- rust2024/other_features/src/main.rs
+++ rust2024/other_features/src/main.rs
@@ -1,85 +1,25 @@
+use std::ops::Add;
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl Add for Point {
+    type Output = Point;
+
+    fn add(self, other: Point) -> Point {
+        Point {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
 fn main() {
-    let mut num = 5;
-
-    let r1 = &raw const num;
-    let r2 = &raw mut num;
-
-    unsafe {
-        println!("r1 is: {}", *r1);
-        println!("r2 is: {}", *r2);
-    }
-
-    unsafe fn dangerous() {}
-
-    unsafe {
-        dangerous();
-    }
-
-    let mut v = vec![1, 2, 3, 4, 5, 6];
-
-    let r = &mut v[..];
-
-    let (a, b) = split_at_mut(r, 3);
-
-    println!("{:?}", a);
-    println!("{:?}", b);
-
-    assert_eq!(a, &mut [1, 2, 3]);
-    assert_eq!(b, &mut [4, 5, 6]);
-
-    println!("Absolute value of -3 according to C: {}", abs(-3));
-
-    println!("value is: {HELLO_WORLD}");
-
-    unsafe {
-        // SAFETY: This is only called from a single thread in `main`.
-        add_to_count(3);
-        println!("COUNTER: {}", *(&raw const COUNTER));
-    }
-}
-
-use std::slice;
-
-fn split_at_mut(values: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
-    let len = values.len();
-    let ptr = values.as_mut_ptr();
-
-    assert!(mid <= len);
-
-    unsafe {
-        (
-            slice::from_raw_parts_mut(ptr, mid),
-            slice::from_raw_parts_mut(ptr.add(mid), len - mid),
-        )
-    }
-}
-
-unsafe extern "C" {
-    safe fn abs(input: i32) -> i32;
-}
-
-#[unsafe(no_mangle)]
-pub extern "C" fn call_from_c() {
-    println!("Just called a Rust function from C!");
-}
-
-static HELLO_WORLD: &str = "Hello, world!";
-
-static mut COUNTER: u32 = 0;
-
-/// SAFETY: Calling this from more than a single thread at a time is undefined
-/// behavior, so you *must* guarantee you only call it from a single thread at
-/// a time.
-unsafe fn add_to_count(inc: u32) {
-    unsafe {
-        COUNTER += inc;
-    }
-}
-
-unsafe trait Foo {
-    // methods go here
-}
-
-unsafe impl Foo for i32 {
-    // method implementations go here
+    assert_eq!(
+        Point { x: 1, y: 0 } + Point { x: 2, y: 3 },
+        Point { x: 3, y: 3 }
+    );
 }
