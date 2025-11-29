9995e7a2c2d2bc12ebd184c5cac573fce83c21c3
jamestheengineer <jamestheengineer@gmail.com>
2025-11-29 09:09:07 -0500

    unit conversion

--- rust2024/other_features/src/main.rs
+++ rust2024/other_features/src/main.rs
@@ -22,4 +22,22 @@ fn main() {
         Point { x: 1, y: 0 } + Point { x: 2, y: 3 },
         Point { x: 3, y: 3 }
     );
+
+    let mm = Millimeters(2000);
+    let m = Meters(2);
+
+    let new_mm = mm + m;
+    println!("{:?}", new_mm);
+}
+
+#[derive(Debug)]
+struct Millimeters(u32);
+struct Meters(u32);
+
+impl Add<Meters> for Millimeters {
+    type Output = Millimeters;
+
+    fn add(self, other: Meters) -> Millimeters {
+        Millimeters(self.0 + (other.0 * 1000))
+    }
 }
