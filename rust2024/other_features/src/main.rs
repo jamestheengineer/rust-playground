c591a90124d65ee8ac9f96c64eed2dec6a957755
jamestheengineer <jamestheengineer@gmail.com>
2025-12-01 19:01:58 -0500

    super traits

--- rust2024/other_features/src/main.rs
+++ rust2024/other_features/src/main.rs
@@ -74,3 +74,25 @@ impl Human {
         println!("*waving arms furiously*");
     }
 }
+
+use std::fmt;
+
+trait OutlinePrint: fmt::Display {
+    fn outline_print(&self) {
+        let output = self.to_string();
+        let len = output.len();
+        println!("{}", "*".repeat(len + 4));
+        println!("*{}*", " ".repeat(len + 2));
+        println!("* {output} *");
+        println!("*{}*", " ".repeat(len + 2));
+        println!("{}", "*".repeat(len + 4));
+    }
+}
+
+impl fmt::Display for Point {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
+    }
+}
+
+impl OutlinePrint for Point {}
