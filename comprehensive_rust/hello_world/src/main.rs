ebe51fe25b25b606a2e09165c87ec4e8dbafd8c7
jamestheengineer <jamestheengineer@gmail.com>
2025-12-26 11:53:01 -0500

    basics

--- comprehensive_rust/hello_world/src/main.rs
+++ comprehensive_rust/hello_world/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
     println!("Hello 🌍!");
+
+    println!("result: {}", interproduct(120, 100, 248));
 }
+
+fn interproduct(a: i32, b: i32, c: i32) -> i32 {
+    return a * b + b * c + c * a;
+}
+
