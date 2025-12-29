6ec2104ee46b8516c3addb4828cc6d054da1f137
jamestheengineer <jamestheengineer@gmail.com>
2025-12-29 14:03:58 -0500

    if statements

--- comprehensive_rust/hello_world/src/main.rs
+++ comprehensive_rust/hello_world/src/main.rs
@@ -14,6 +14,19 @@ fn main() {
     };
     dbg!(x);
     // dbg!(y);
+
+    let x = 10;
+    if x == 0 {
+        println!("zero!");
+    } else if x < 100 {
+        println!("biggish");
+    } else {
+        println!("huge");
+    }
+
+    let x = 10;
+    let size = if x < 20 { "small" } else { "large" };
+    println!("number size: {}", size);
 }
 
 fn interproduct(a: i32, b: i32, c: i32) -> i32 {
