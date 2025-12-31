a46246fa9b00a0586fb7d830bd4618391d2966df
jamestheengineer <jamestheengineer@gmail.com>
2025-12-31 10:49:59 -0500

    breaking out of nested loops

--- comprehensive_rust/hello_world/src/main.rs
+++ comprehensive_rust/hello_world/src/main.rs
@@ -22,7 +22,7 @@ fn main() {
         }
     }
 
-     let mut i = 0;
+    let mut i = 0;
     loop {
         i += 1;
         if i > 5 {
@@ -33,4 +33,17 @@ fn main() {
         }
         dbg!(i);
     }
+
+    let s = [[5, 6, 7], [8, 9, 10], [21, 15, 32]];
+    let mut elements_searched = 0;
+    let target_value = 10;
+    'outer: for i in 0..=2 {
+        for j in 0..=2 {
+            elements_searched += 1;
+            if s[i][j] == target_value {
+                break 'outer;
+            }
+        }
+    }
+    dbg!(elements_searched);
 }
