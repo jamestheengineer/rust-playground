e9ffea8609d5e4aba5d3ec7cec44b66f15cf8920
jamestheengineer <jamestheengineer@gmail.com>
2026-01-01 10:32:39 -0500

    fns and macros

--- comprehensive_rust/hello_world/src/main.rs
+++ comprehensive_rust/hello_world/src/main.rs
@@ -1,49 +1,16 @@
+fn factorial(n: u32) -> u32 {
+    let mut product = 1;
+    for i in 1..=n {
+        product *= dbg!(i);
+    }
+    product
+}
+
+fn fizzbuzz(n: u32) -> u32 {
+    todo!()
+}
+
 fn main() {
-    let mut x = 200;
-    while x >= 10 {
-        x = x / 2;
-    }
-    dbg!(x);
-
-    for x in 1..=5 {
-        dbg!(x);
-    }
-
-    for elem in [2, 4, 8, 16, 32] {
-        dbg!(elem);
-    }
-
-    let mut i = 0;
-    loop {
-        i += 1;
-        dbg!(i);
-        if i > 100 {
-            break;
-        }
-    }
-
-    let mut i = 0;
-    loop {
-        i += 1;
-        if i > 5 {
-            break;
-        }
-        if i % 2 == 0 {
-            continue;
-        }
-        dbg!(i);
-    }
-
-    let s = [[5, 6, 7], [8, 9, 10], [21, 15, 32]];
-    let mut elements_searched = 0;
-    let target_value = 10;
-    'outer: for i in 0..=2 {
-        for j in 0..=2 {
-            elements_searched += 1;
-            if s[i][j] == target_value {
-                break 'outer;
-            }
-        }
-    }
-    dbg!(elements_searched);
+    let n = 4;
+    println!("{n}! = {}", fizzbuzz(n));
 }
