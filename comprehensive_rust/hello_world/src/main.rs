b037be8b99c4e80d2a7caadacf8a544c33fd041e
jamestheengineer <jamestheengineer@gmail.com>
2026-01-02 11:00:20 -0500

    collatz length

--- comprehensive_rust/hello_world/src/main.rs
+++ comprehensive_rust/hello_world/src/main.rs
@@ -1,16 +1,20 @@
-fn factorial(n: u32) -> u32 {
-    let mut product = 1;
-    for i in 1..=n {
-        product *= dbg!(i);
+/// Determine the length of the collatz sequence beginning at `n`.
+fn collatz_length(mut n: i32) -> u32 {
+    let mut length = 1;
+    while n != 1 {
+        n = collatz_step(n);
+        length += 1;
     }
-    product
+    length
 }
-
-fn fizzbuzz(n: u32) -> u32 {
-    todo!()
+fn collatz_step(n: i32) -> i32 {
+    if n % 2 == 0 {
+        n / 2
+    } else {
+        3 * n + 1
+    }
 }
 
 fn main() {
-    let n = 4;
-    println!("{n}! = {}", fizzbuzz(n));
+    println!("Length: {}", collatz_length(11)); // should be 15
 }
