dd5a9a93d5c2afdca3b5ac9b0ec2666d3864398f
jamestheengineer <jamestheengineer@gmail.com>
2026-01-03 11:34:24 -0500

    arrays are on the stack

--- comprehensive_rust/hello_world/src/main.rs
+++ comprehensive_rust/hello_world/src/main.rs
@@ -1,30 +1,5 @@
-/// Determine the length of the collatz sequence beginning at `n`.
-fn collatz_length(mut n: i32) -> u32 {
-    let mut length = 1;
-    while n != 1 {
-        n = collatz_step(n);
-        length += 1;
-    }
-    length
-}
-fn collatz_step(n: i32) -> i32 {
-    if n % 2 == 0 {
-        n / 2
-    } else {
-        3 * n + 1
-    }
-}
-
-fn collatz_length2(mut n: i32) -> u32 {
-    let mut len = 1;
-    while n > 1 {
-        n = if n % 2 == 0 { n / 2 } else { 3 * n + 1 };
-        len += 1;
-    }
-    len
-}
-
 fn main() {
-    println!("Length: {}", collatz_length(11)); // should be 15
-    println!("Length: {}", collatz_length2(11)); // should be 15
+    let mut a: [i8; 5] = [5, 4, 3, 2, 1];
+    a[2] = 0;
+    println!("a: {a:?}");
 }
