e63edc58c0cd35719fb465d49f95ed1b18350c10
jamestheengineer <jamestheengineer@gmail.com>
2026-01-04 11:17:12 -0500

    tuples and arrays

--- comprehensive_rust/hello_world/src/main.rs
+++ comprehensive_rust/hello_world/src/main.rs
@@ -1,5 +1,28 @@
 fn main() {
-    let mut a: [i8; 5] = [5, 4, 3, 2, 1];
-    a[2] = 0;
-    println!("a: {a:?}");
+    let t: (i8, bool) = (7, true);
+    dbg!(t.0);
+    dbg!(t.1);
+
+    let primes = [2, 3, 5, 7, 11, 13, 17, 19];
+    for prime in primes {
+        for i in 2..prime {
+            assert_ne!(prime % i, 0);
+            dbg!(prime, i);
+        }
+    }
+
+    let tuple = (1, 5, 3);
+    println!(
+        "{tuple:?}: {}",
+        if check_order(tuple) {
+            "ordered"
+        } else {
+            "unordered"
+        }
+    );
+}
+
+fn check_order(tuple: (i32, i32, i32)) -> bool {
+    let (left, middle, right) = tuple;
+    left < middle && middle < right
 }
