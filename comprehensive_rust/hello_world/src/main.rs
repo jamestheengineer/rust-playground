304ffe4d74191a66d1ee067e5ae18abf2342d22b
jamestheengineer <jamestheengineer@gmail.com>
2026-01-05 18:45:04 -0500

    transpose

--- comprehensive_rust/hello_world/src/main.rs
+++ comprehensive_rust/hello_world/src/main.rs
@@ -1,28 +1,29 @@
-fn main() {
-    let t: (i8, bool) = (7, true);
-    dbg!(t.0);
-    dbg!(t.1);
-
-    let primes = [2, 3, 5, 7, 11, 13, 17, 19];
-    for prime in primes {
-        for i in 2..prime {
-            assert_ne!(prime % i, 0);
-            dbg!(prime, i);
+fn transpose(matrix: [[i32; 3]; 3]) -> [[i32; 3]; 3] {
+    let mut result = [[0; 3]; 3];
+    for i in 0..3 {
+        for j in 0..3 {
+            result[i][j] = matrix[j][i];
         }
     }
-
-    let tuple = (1, 5, 3);
-    println!(
-        "{tuple:?}: {}",
-        if check_order(tuple) {
-            "ordered"
-        } else {
-            "unordered"
-        }
-    );
+    result
 }
 
-fn check_order(tuple: (i32, i32, i32)) -> bool {
-    let (left, middle, right) = tuple;
-    left < middle && middle < right
+fn main() {
+    let matrix = [
+        [101, 102, 103], // <-- the comment makes rustfmt add a newline
+        [201, 202, 203],
+        [301, 302, 303],
+    ];
+
+    println!("Original:");
+    for row in &matrix {
+        println!("{:?}", row);
+    }
+
+    let transposed = transpose(matrix);
+
+    println!("\nTransposed:");
+    for row in &transposed {
+        println!("{:?}", row);
+    }
 }
