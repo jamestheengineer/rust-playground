80f0e7996e4bc6b45a5a52d4f7119adffb31950b
jamestheengineer <jamestheengineer@gmail.com>
2026-01-12 16:22:57 -0500

    structs

--- comprehensive_rust/hello_world/src/main.rs
+++ comprehensive_rust/hello_world/src/main.rs
@@ -1,25 +1,24 @@
-/// Calculate the magnitude of the given vector.
-fn magnitude(vector: &[f64; 3]) -> f64 {
-    let mut mag_squared = 0.0;
-    for coord in vector {
-        mag_squared += coord * coord;
-    }
-    mag_squared.sqrt()
+struct Person {
+    name: String,
+    age: u8,
 }
 
-/// Change the magnitude of the vector to 1.0 without changing its direction.
-fn normalize(vector: &mut [f64; 3]) {
-    let mag = magnitude(vector);
-    for item in vector {
-        *item /= mag;
-    }
+fn describe(person: &Person) {
+    println!("{} is {} years old", person.name, person.age);
 }
 
 fn main() {
-    println!("Magnitude of a unit vector: {}", magnitude(&[0.0, 1.0, 0.0]));
+    let mut peter = Person {
+        name: String::from("Peter"),
+        age: 27,
+    };
+    describe(&peter);
 
-    let mut v = [1.0, 2.0, 9.0];
-    println!("Magnitude of {v:?}: {}", magnitude(&v));
-    normalize(&mut v);
-    println!("Magnitude of {v:?} after normalization: {}", magnitude(&v));
+    peter.age = 28;
+    describe(&peter);
+
+    let name = String::from("Avery");
+    let age = 39;
+    let avery = Person { name, age };
+    describe(&avery);
 }
