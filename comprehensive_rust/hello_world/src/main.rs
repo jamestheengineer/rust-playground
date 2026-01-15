66c553cfb4983aa65e794de25973c243d4a42123
jamestheengineer <jamestheengineer@gmail.com>
2026-01-15 18:04:24 -0500

    enums

--- comprehensive_rust/hello_world/src/main.rs
+++ comprehensive_rust/hello_world/src/main.rs
@@ -1,6 +1,18 @@
-struct Point(i32, i32);
+#[derive(Debug)]
+enum Direction {
+    Left,
+    Right,
+}
+
+#[derive(Debug)]
+enum PlayerMove {
+    Pass,                        // Simple variant
+    Run(Direction),              // Tuple variant
+    Teleport { x: u32, y: u32 }, // Struct variant
+}
 
 fn main() {
-    let p = Point(17, 23);
-    println!("({}, {})", p.0, p.1);
+    let dir = Direction::Left;
+    let player_move: PlayerMove = PlayerMove::Run(dir);
+    println!("On this turn: {player_move:?}");
 }
