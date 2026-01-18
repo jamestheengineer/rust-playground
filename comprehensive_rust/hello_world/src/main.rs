25e407a139cec62e72576dbdd45fb1f816774e42
jamestheengineer <jamestheengineer@gmail.com>
2026-01-18 13:25:58 -0500

    destructure reminder

--- comprehensive_rust/hello_world/src/main.rs
+++ comprehensive_rust/hello_world/src/main.rs
@@ -1,77 +1,21 @@
-#![allow(dead_code)]
+fn takes_tuple(tuple: (char, i32, bool)) {
+    let a = tuple.0;
+    let b = tuple.1;
+    let c = tuple.2;
 
-#[derive(Debug)]
-/// An event in the elevator system that the controller must react to.
-enum Event {
-    /// A button was pressed.
-    ButtonPressed(Button),
+    // This does the same thing as above.
+    let (a, b, c) = tuple;
+    println!("a: {}, b: {}, c: {}", a, b, c);
 
-    /// The car has arrived at the given floor.
-    CarArrived(Floor),
+    // Ignore the first element, only bind the second and third.
+    let (_, b, c) = tuple;
+    println!("a: {}, b: {}, c: {}", a, b, c);
 
-    /// The car's doors have opened.
-    CarDoorOpened,
-
-    /// The car's doors have closed.
-    CarDoorClosed,
-}
-
-/// A floor is represented as an integer.
-type Floor = i32;
-
-/// A direction of travel.
-#[derive(Debug)]
-enum Direction {
-    Up,
-    Down,
-}
-
-/// A user-accessible button.
-#[derive(Debug)]
-enum Button {
-    /// A button in the elevator lobby on the given floor.
-    LobbyCall(Direction, Floor),
-
-    /// A floor button within the car.
-    CarFloor(Floor),
-}
-
-/// The car has arrived on the given floor.
-fn car_arrived(floor: i32) -> Event {
-    Event::CarArrived(floor)
-}
-
-/// The car doors have opened.
-fn car_door_opened() -> Event {
-    Event::CarDoorOpened
-}
-
-/// The car doors have closed.
-fn car_door_closed() -> Event {
-    Event::CarDoorClosed
-}
-
-/// A directional button was pressed in an elevator lobby on the given floor.
-fn lobby_call_button_pressed(floor: i32, dir: Direction) -> Event {
-    Event::ButtonPressed(Button::LobbyCall(dir, floor))
-}
-
-/// A floor button was pressed in the elevator car.
-fn car_floor_button_pressed(floor: i32) -> Event {
-    Event::ButtonPressed(Button::CarFloor(floor))
+    // Ignore everything but the last element.
+    let (.., c) = tuple;
+    println!("a: {}, b: {}, c: {}", a, b, c);
 }
 
 fn main() {
-    println!(
-        "A ground floor passenger has pressed the up button: {:?}",
-        lobby_call_button_pressed(0, Direction::Up)
-    );
-    println!("The car has arrived on the ground floor: {:?}", car_arrived(0));
-    println!("The car door opened: {:?}", car_door_opened());
-    println!(
-        "A passenger has pressed the 3rd floor button: {:?}",
-        car_floor_button_pressed(3)
-    );
-    println!("The car door closed: {:?}", car_door_closed());
-    println!("The car has arrived on the 3rd floor: {:?}", car_arrived(3));
-}
+    takes_tuple(('a', 777, true));
+} 
