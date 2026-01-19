863cce8413620cd54f45e05da88cdb57a6f61341
jamestheengineer <jamestheengineer@gmail.com>
2026-01-19 18:02:22 -0500

    matching

--- comprehensive_rust/hello_world/src/main.rs
+++ comprehensive_rust/hello_world/src/main.rs
@@ -1,21 +1,11 @@
-fn takes_tuple(tuple: (char, i32, bool)) {
-    let a = tuple.0;
-    let b = tuple.1;
-    let c = tuple.2;
-
-    // This does the same thing as above.
-    let (a, b, c) = tuple;
-    println!("a: {}, b: {}, c: {}", a, b, c);
-
-    // Ignore the first element, only bind the second and third.
-    let (_, b, c) = tuple;
-    println!("a: {}, b: {}, c: {}", a, b, c);
-
-    // Ignore everything but the last element.
-    let (.., c) = tuple;
-    println!("a: {}, b: {}, c: {}", a, b, c);
-}
-
+#[rustfmt::skip]
 fn main() {
-    takes_tuple(('a', 777, true));
-} 
+    let input = 'x';
+    match input {
+        'q'                       => println!("Quitting"),
+        'a' | 's' | 'w' | 'd'     => println!("Moving around"),
+        '0'..='9'                 => println!("Number input"),
+        key if key.is_lowercase() => println!("Lowercase: {key}"),
+        _                         => println!("Something else"),
+    }
+}
