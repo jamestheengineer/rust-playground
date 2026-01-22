95ecefcb65d13576fe8ee79e8796d02cd19d6860
jamestheengineer <jamestheengineer@gmail.com>
2026-01-22 17:24:38 -0500

    enums

--- comprehensive_rust/hello_world/src/main.rs
+++ comprehensive_rust/hello_world/src/main.rs
@@ -1,14 +1,20 @@
-struct Foo {
-    x: (u32, u32),
-    y: u32,
+enum Result {
+    Ok(i32),
+    Err(String),
 }
 
-#[rustfmt::skip]
+fn divide_in_two(n: i32) -> Result {
+    if n % 2 == 0 {
+        Result::Ok(n / 2)
+    } else {
+        Result::Err(format!("cannot divide {n} into two equal parts"))
+    }
+}
+
 fn main() {
-    let foo = Foo { x: (2, 2), y: 5 };
-    match foo {
-        Foo { y: 2, x: i }   => println!("y = 2, x = {i:?}"),
-        Foo { x: (1, b), y } => println!("x.0 = 1, b = {b}, y = {y}"),
-        Foo { y, .. }        => println!("y = {y}, other fields were ignored"),
+    let n = 100;
+    match divide_in_two(n) {
+        Result::Ok(half) => println!("{n} divided in two is {half}"),
+        Result::Err(msg) => println!("sorry, an error happened: {msg}"),
     }
 }
