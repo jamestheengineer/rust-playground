b5ec8597b75c0b3712619f63c5f347fadb95a63b
jamestheengineer <jamestheengineer@gmail.com>
2026-01-24 07:57:50 -0500

    while let

--- comprehensive_rust/hello_world/src/main.rs
+++ comprehensive_rust/hello_world/src/main.rs
@@ -1,15 +1,7 @@
-use std::time::Duration;
-
-fn sleep_for(secs: f32) {
-    let result = Duration::try_from_secs_f32(secs);
-
-    if let Ok(duration) = result {
-        std::thread::sleep(duration);
-        println!("slept for {duration:?}");
-    }
-}
-
 fn main() {
-    sleep_for(-10.0);
-    sleep_for(0.8);
+    let mut name = String::from("Comprehensive Rust 🦀");
+    while let Some(c) = name.pop() {
+        dbg!(c);
+    }
+    // (There are more efficient ways to reverse a string!)
 }
