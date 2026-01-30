cf02b7b4c18777e6cc621c0ccbf5ab6cbf4ffcd4
jamestheengineer <jamestheengineer@gmail.com>
2026-01-30 18:11:17 -0500

    associated types

--- comprehensive_rust/hello_world/src/main.rs
+++ comprehensive_rust/hello_world/src/main.rs
@@ -1,26 +1,20 @@
-trait Animal {
-    fn leg_count(&self) -> u32;
+#[derive(Debug)]
+struct Meters(i32);
+#[derive(Debug)]
+struct MetersSquared(i32);
+
+trait Multiply {
+    type Output;
+    fn multiply(&self, other: &Self) -> Self::Output;
 }
 
-trait Pet: Animal {
-    fn name(&self) -> String;
-}
-
-struct Dog(String);
-
-impl Animal for Dog {
-    fn leg_count(&self) -> u32 {
-        4
-    }
-}
-
-impl Pet for Dog {
-    fn name(&self) -> String {
-        self.0.clone()
+impl Multiply for Meters {
+    type Output = MetersSquared;
+    fn multiply(&self, other: &Self) -> Self::Output {
+        MetersSquared(self.0 * other.0)
     }
 }
 
 fn main() {
-    let puppy = Dog(String::from("Rex"));
-    println!("{} has {} legs", puppy.name(), puppy.leg_count());
+    println!("{:?}", Meters(10).multiply(&Meters(20)));
 }
