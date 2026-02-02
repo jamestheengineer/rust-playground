6798f716f3e6cf1cb14eb27f6e38942f1ff7c9a3
jamestheengineer <jamestheengineer@gmail.com>
2026-02-02 17:08:46 -0500

    generics

--- comprehensive_rust/hello_world/src/main.rs
+++ comprehensive_rust/hello_world/src/main.rs
@@ -1,32 +1,8 @@
-trait Logger {
-    /// Log a message at the given verbosity level.
-    fn log(&self, verbosity: u8, message: &str);
-}
-
-struct StderrLogger;
-
-impl Logger for StderrLogger {
-    fn log(&self, verbosity: u8, message: &str) {
-        eprintln!("verbosity={verbosity}: {message}");
-    }
-}
-
-/// Only log messages up to the given verbosity level.
-struct VerbosityFilter {
-    max_verbosity: u8,
-    inner: StderrLogger,
-}
-
-impl Logger for VerbosityFilter {
-    fn log(&self, verbosity: u8, message: &str) {
-        if verbosity <= self.max_verbosity {
-            self.inner.log(verbosity, message);
-        }
-    }
+fn pick<T>(cond: bool, left: T, right: T) -> T {
+    if cond { left } else { right }
 }
 
 fn main() {
-    let logger = VerbosityFilter { max_verbosity: 3, inner: StderrLogger };
-    logger.log(5, "FYI");
-    logger.log(2, "Uhoh");
+    println!("picked a number: {:?}", pick(true, 222, 333));
+    println!("picked a string: {:?}", pick(false, 'L', 'R'));
 }
