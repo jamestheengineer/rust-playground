78c866dff30ce6251c92562c551bc79e7b22cd68
jamestheengineer <jamestheengineer@gmail.com>
2026-02-04 18:31:54 -0500

    more traits

--- comprehensive_rust/hello_world/src/main.rs
+++ comprehensive_rust/hello_world/src/main.rs
@@ -1,11 +1,32 @@
-fn duplicate<T: Clone>(a: T) -> (T, T) {
-    (a.clone(), a.clone())
+pub trait Logger {
+    /// Log a message at the given verbosity level.
+    fn log(&self, verbosity: u8, message: &str);
 }
 
-struct NotCloneable;
+struct StderrLogger;
+
+impl Logger for StderrLogger {
+    fn log(&self, verbosity: u8, message: &str) {
+        eprintln!("verbosity={verbosity}: {message}");
+    }
+}
+
+/// Only log messages up to the given verbosity level.
+struct VerbosityFilter<L> {
+    max_verbosity: u8,
+    inner: L,
+}
+
+impl<L: Logger> Logger for VerbosityFilter<L> {
+    fn log(&self, verbosity: u8, message: &str) {
+        if verbosity <= self.max_verbosity {
+            self.inner.log(verbosity, message);
+        }
+    }
+}
 
 fn main() {
-    let foo = String::from("foo");
-    let pair = duplicate(foo);
-    println!("{pair:?}");
+    let logger = VerbosityFilter { max_verbosity: 3, inner: StderrLogger };
+    logger.log(5, "FYI");
+    logger.log(2, "Uhoh");
 }
