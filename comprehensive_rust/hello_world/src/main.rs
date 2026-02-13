bc33e2e0970a51a285e15582068af99e6c3cfd72
jamestheengineer <jamestheengineer@gmail.com>
2026-02-12 19:04:20 -0500

    exercise...not sure I get all of it

--- comprehensive_rust/hello_world/src/main.rs
+++ comprehensive_rust/hello_world/src/main.rs
@@ -1,30 +1,46 @@
-fn apply_and_log(
-    func: impl FnOnce(&'static str) -> String,
-    func_name: &'static str,
-    input: &'static str,
-) {
-    println!("Calling {func_name}({input}): {}", func(input))
+pub trait Logger {
+    /// Log a message at the given verbosity level.
+    fn log(&self, verbosity: u8, message: &str);
+}
+
+struct StderrLogger;
+
+impl Logger for StderrLogger {
+    fn log(&self, verbosity: u8, message: &str) {
+        eprintln!("verbosity={verbosity}: {message}");
+    }
+}
+
+/// Only log messages matching a filtering predicate.
+struct Filter<L, P> {
+    inner: L,
+    predicate: P,
+}
+
+impl<L, P> Filter<L, P>
+where
+    L: Logger,
+    P: Fn(u8, &str) -> bool,
+{
+    fn new(inner: L, predicate: P) -> Self {
+        Self { inner, predicate }
+    }
+}
+impl<L, P> Logger for Filter<L, P>
+where
+    L: Logger,
+    P: Fn(u8, &str) -> bool,
+{
+    fn log(&self, verbosity: u8, message: &str) {
+        if (self.predicate)(verbosity, message) {
+            self.inner.log(verbosity, message);
+        }
+    }
 }
 
 fn main() {
-    let suffix = "-itis";
-    let add_suffix = |x| format!("{x}{suffix}");
-    apply_and_log(&add_suffix, "add_suffix", "senior");
-    apply_and_log(&add_suffix, "add_suffix", "appendix");
-
-    let mut v = Vec::new();
-    let mut accumulate = |x| {
-        v.push(x);
-        v.join("/")
-    };
-    apply_and_log(&mut accumulate, "accumulate", "red");
-    apply_and_log(&mut accumulate, "accumulate", "green");
-    apply_and_log(&mut accumulate, "accumulate", "blue");
-
-    let take_and_reverse = |prefix| {
-        let mut acc = String::from(prefix);
-        acc.push_str(&v.into_iter().rev().collect::<Vec<_>>().join("/"));
-        acc
-    };
-    apply_and_log(take_and_reverse, "take_and_reverse", "reversed: ");
+    let logger = Filter::new(StderrLogger, |_verbosity, msg| msg.contains("yikes"));
+    logger.log(5, "FYI");
+    logger.log(1, "yikes, something went wrong");
+    logger.log(2, "uhoh");
 }
