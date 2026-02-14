87f9137613869ca231a05d7c1629a3de85461d22
jamestheengineer <jamestheengineer@gmail.com>
2026-02-14 07:57:27 -0500

    Option

--- comprehensive_rust/hello_world/src/main.rs
+++ comprehensive_rust/hello_world/src/main.rs
@@ -1,46 +1,9 @@
-pub trait Logger {
-    /// Log a message at the given verbosity level.
-    fn log(&self, verbosity: u8, message: &str);
-}
-
-struct StderrLogger;
-
-impl Logger for StderrLogger {
-    fn log(&self, verbosity: u8, message: &str) {
-        eprintln!("verbosity={verbosity}: {message}");
-    }
-}
-
-/// Only log messages matching a filtering predicate.
-struct Filter<L, P> {
-    inner: L,
-    predicate: P,
-}
-
-impl<L, P> Filter<L, P>
-where
-    L: Logger,
-    P: Fn(u8, &str) -> bool,
-{
-    fn new(inner: L, predicate: P) -> Self {
-        Self { inner, predicate }
-    }
-}
-impl<L, P> Logger for Filter<L, P>
-where
-    L: Logger,
-    P: Fn(u8, &str) -> bool,
-{
-    fn log(&self, verbosity: u8, message: &str) {
-        if (self.predicate)(verbosity, message) {
-            self.inner.log(verbosity, message);
-        }
-    }
-}
-
 fn main() {
-    let logger = Filter::new(StderrLogger, |_verbosity, msg| msg.contains("yikes"));
-    logger.log(5, "FYI");
-    logger.log(1, "yikes, something went wrong");
-    logger.log(2, "uhoh");
+    let name = "Löwe 老虎 Léopard Gepardi";
+    let mut position: Option<usize> = name.find('é');
+    dbg!(position);
+    assert_eq!(position.unwrap(), 14);
+    position = name.find('Z');
+    dbg!(position);
+    assert_eq!(position.expect("Character not found"), 0);
 }
