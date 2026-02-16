e462a2c6d58f70bd123a5b90356f799192f7ada0
jamestheengineer <jamestheengineer@gmail.com>
2026-02-16 15:40:23 -0500

    strings

--- comprehensive_rust/hello_world/src/main.rs
+++ comprehensive_rust/hello_world/src/main.rs
@@ -1,19 +1,13 @@
-use std::fs::File;
-use std::io::Read;
-
 fn main() {
-    let file: Result<File, std::io::Error> = File::open("diary.txt");
-    match file {
-        Ok(mut file) => {
-            let mut contents = String::new();
-            if let Ok(bytes) = file.read_to_string(&mut contents) {
-                println!("Dear diary: {contents} ({bytes} bytes)");
-            } else {
-                println!("Could not read file content");
-            }
-        }
-        Err(err) => {
-            println!("The diary could not be opened: {err}");
-        }
-    }
+    let mut s1 = String::new();
+    s1.push_str("Hello");
+    println!("s1: len = {}, capacity = {}", s1.len(), s1.capacity());
+
+    let mut s2 = String::with_capacity(s1.len() + 1);
+    s2.push_str(&s1);
+    s2.push('!');
+    println!("s2: len = {}, capacity = {}", s2.len(), s2.capacity());
+
+    let s3 = String::from("🇨🇭");
+    println!("s3: len = {}, number of chars = {}", s3.len(), s3.chars().count());
 }
