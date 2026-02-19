5bec2a460b839fd2e93df139b0e8021a17f7b25d
jamestheengineer <jamestheengineer@gmail.com>
2026-02-18 20:31:55 -0500

    hash maps

--- comprehensive_rust/hello_world/src/main.rs
+++ comprehensive_rust/hello_world/src/main.rs
@@ -1,21 +1,33 @@
+// Copyright 2023 Google LLC
+// SPDX-License-Identifier: Apache-2.0
+
+use std::collections::HashMap;
+
 fn main() {
-    let mut v1 = Vec::new();
-    v1.push(42);
-    println!("v1: len = {}, capacity = {}", v1.len(), v1.capacity());
+    let mut page_counts = HashMap::new();
+    page_counts.insert("Adventures of Huckleberry Finn", 207);
+    page_counts.insert("Grimms' Fairy Tales", 751);
+    page_counts.insert("Pride and Prejudice", 303);
 
-    let mut v2 = Vec::with_capacity(v1.len() + 1);
-    v2.extend(v1.iter());
-    v2.push(9999);
-    println!("v2: len = {}, capacity = {}", v2.len(), v2.capacity());
+    if !page_counts.contains_key("Les Misérables") {
+        println!(
+            "We know about {} books, but not Les Misérables.",
+            page_counts.len()
+        );
+    }
 
-    // Canonical macro to initialize a vector with elements.
-    let mut v3 = vec![0, 0, 1, 2, 3, 4];
+    for book in ["Pride and Prejudice", "Alice's Adventure in Wonderland"] {
+        match page_counts.get(book) {
+            Some(count) => println!("{book}: {count} pages"),
+            None => println!("{book} is unknown."),
+        }
+    }
 
-    // Retain only the even elements.
-    v3.retain(|x| x % 2 == 0);
-    println!("{v3:?}");
+    // Use the .entry() method to insert a value if nothing is found.
+    for book in ["Pride and Prejudice", "Alice's Adventure in Wonderland"] {
+        let page_count: &mut i32 = page_counts.entry(book).or_insert(0);
+        *page_count += 1;
+    }
 
-    // Remove consecutive duplicates.
-    v3.dedup();
-    println!("{v3:?}");
-} 
+    dbg!(page_counts);
+}
