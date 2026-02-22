d63d37d4975cdf3db929a52a6b590f6edaa78fea
jamestheengineer <jamestheengineer@gmail.com>
2026-02-22 08:54:26 -0500

    standard traits

--- comprehensive_rust/hello_world/src/main.rs
+++ comprehensive_rust/hello_world/src/main.rs
@@ -1,47 +1,36 @@
 // Copyright 2023 Google LLC
 // SPDX-License-Identifier: Apache-2.0
 
-use std::collections::HashMap;
-use std::hash::Hash;
-
-/// Counter counts the number of times each value of type T has been seen.
-struct Counter<T> {
-    values: HashMap<T, u64>,
+struct Key {
+    id: u32,
+    metadata: Option<String>,
+}
+impl PartialEq for Key {
+    fn eq(&self, other: &Self) -> bool {
+        self.id == other.id
+    }
 }
 
-impl<T: Eq + Hash> Counter<T> {
-    /// Create a new Counter.
-    fn new() -> Self {
-        Counter { values: HashMap::new() }
-    }
+// Copyright 2023 Google LLC
+// SPDX-License-Identifier: Apache-2.0
 
-    /// Count an occurrence of the given value.
-    fn count(&mut self, value: T) {
-        *self.values.entry(value).or_default() += 1;
-    }
-
-    /// Return the number of times the given value has been seen.
-    fn times_seen(&self, value: T) -> u64 {
-        self.values.get(&value).copied().unwrap_or_default()
-    }
+use std::cmp::Ordering;
+#[derive(Eq, PartialEq)]
+struct Citation {
+    author: String,
+    year: u32,
 }
+impl PartialOrd for Citation {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        match self.author.partial_cmp(&other.author) {
+            Some(Ordering::Equal) => self.year.partial_cmp(&other.year),
+            author_ord => author_ord,
+        }
+    }
+}   
 
 fn main() {
-    let mut ctr = Counter::new();
-    ctr.count(13);
-    ctr.count(14);
-    ctr.count(16);
-    ctr.count(14);
-    ctr.count(14);
-    ctr.count(11);
-
-    for i in 10..20 {
-        println!("saw {} values equal to {}", ctr.times_seen(i), i);
-    }
-
-    let mut strctr = Counter::new();
-    strctr.count("apple");
-    strctr.count("orange");
-    strctr.count("apple");
-    println!("got {} apples", strctr.times_seen("apple"));
+    let a = Citation {author: String::from("Smith"), year: 2020};
+    let b = Citation {author: String::from("Nelson"), year: 2021};
+    assert!(a < b);
 }
