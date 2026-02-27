cc5d487a8e404fc90fc1f14771aa1447183d3f11
jamestheengineer <jamestheengineer@gmail.com>
2026-02-27 17:11:49 -0500

    derived

--- comprehensive_rust/hello_world/src/main.rs
+++ comprehensive_rust/hello_world/src/main.rs
@@ -1,29 +1,30 @@
 // Copyright 2023 Google LLC
 // SPDX-License-Identifier: Apache-2.0
 
-use std::io::Write;
-use std::io::{BufRead, BufReader, Read, Result};
-
-fn log<W: Write>(writer: &mut W, msg: &str) -> Result<()> {
-    writer.write_all(msg.as_bytes())?;
-    writer.write_all("\n".as_bytes())
+#[derive(Debug, Default)]
+struct Derived {
+    x: u32,
+    y: String,
+    z: Implemented,
 }
 
-fn count_lines<R: Read>(reader: R) -> usize {
-    let buf_reader = BufReader::new(reader);
-    buf_reader.lines().count()
+#[derive(Debug)]
+struct Implemented(String);
+
+impl Default for Implemented {
+    fn default() -> Self {
+        Self("John Smith".into())
+    }
 }
 
-fn main() -> Result<()> {
-    let slice: &[u8] = b"foo\nbar\nbaz\n";
-    println!("lines in slice: {}", count_lines(slice));
+fn main() {
+    let default_struct = Derived::default();
+    dbg!(default_struct);
 
-    let file = std::fs::File::open(std::env::current_exe()?)?;
-    println!("lines in file: {}", count_lines(file));
+    let almost_default_struct =
+        Derived { y: "Y is set!".into(), ..Derived::default() };
+    dbg!(almost_default_struct);
 
-    let mut buffer = Vec::new();
-    log(&mut buffer, "Hello")?;
-    log(&mut buffer, "World")?;
-    println!("Logged: {buffer:?}");
-    Ok(())
+    let nothing: Option<Derived> = None;
+    dbg!(nothing.unwrap_or_default());
 }
