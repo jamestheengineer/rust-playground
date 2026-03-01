7439b4eb98716ac2b5e29cd7eb12e1d8359a0e0a
jamestheengineer <jamestheengineer@gmail.com>
2026-03-01 08:38:23 -0500

    memory

--- comprehensive_rust/hello_world/src/main.rs
+++ comprehensive_rust/hello_world/src/main.rs
@@ -1,50 +1,15 @@
 // Copyright 2023 Google LLC
 // SPDX-License-Identifier: Apache-2.0
 
-use std::io::Read;
-
-struct RotDecoder<R: Read> {
-    input: R,
-    rot: u8,
-}
-
-impl<R: Read> Read for RotDecoder<R> {
-    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
-        let size = self.input.read(buf)?;
-        for b in &mut buf[..size] {
-            if b.is_ascii_alphabetic() {
-                let base = if b.is_ascii_uppercase() { 'A' } else { 'a' } as u8;
-                *b = (*b - base + self.rot) % 26 + base;
-            }
-        }
-        Ok(size)
-    }
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn joke() {
-        let mut rot =
-            RotDecoder { input: "Gb trg gb gur bgure fvqr!".as_bytes(), rot: 13 };
-        let mut result = String::new();
-        rot.read_to_string(&mut result).unwrap();
-        assert_eq!(&result, "To get to the other side!");
-    }
-
-    #[test]
-    fn binary() {
-        let input: Vec<u8> = (0..=255u8).collect();
-        let mut rot = RotDecoder::<&[u8]> { input: input.as_slice(), rot: 13 };
-        let mut buf = [0u8; 256];
-        assert_eq!(rot.read(&mut buf).unwrap(), 256);
-        for i in 0..=255 {
-            if input[i] != buf[i] {
-                assert!(input[i].is_ascii_alphabetic());
-                assert!(buf[i].is_ascii_alphabetic());
-            }
-        }
+fn main() {
+    let mut s1 = String::from("Hello");
+    s1.push(' ');
+    s1.push_str("world");
+    // DON'T DO THIS AT HOME! For educational purposes only.
+    // String provides no guarantees about its layout, so this could lead to
+    // undefined behavior.
+    unsafe {
+        let (capacity, ptr, len): (usize, usize, usize) = std::mem::transmute(s1);
+        println!("capacity = {capacity}, ptr = {ptr:#x}, len = {len}");
     }
 }
