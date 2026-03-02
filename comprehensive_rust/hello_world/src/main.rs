0f5dbb1b1dfa9bd8798550fc724ac0a5cadb5607
jamestheengineer <jamestheengineer@gmail.com>
2026-03-02 18:48:41 -0500

    more moves

--- comprehensive_rust/hello_world/src/main.rs
+++ comprehensive_rust/hello_world/src/main.rs
@@ -1,15 +1,12 @@
 // Copyright 2023 Google LLC
 // SPDX-License-Identifier: Apache-2.0
 
+fn say_hello(name: String) {
+    println!("Hello {name}")
+}
+
 fn main() {
-    let mut s1 = String::from("Hello");
-    s1.push(' ');
-    s1.push_str("world");
-    // DON'T DO THIS AT HOME! For educational purposes only.
-    // String provides no guarantees about its layout, so this could lead to
-    // undefined behavior.
-    unsafe {
-        let (capacity, ptr, len): (usize, usize, usize) = std::mem::transmute(s1);
-        println!("capacity = {capacity}, ptr = {ptr:#x}, len = {len}");
-    }
+    let name = String::from("Alice");
+    say_hello(name);
+    // say_hello(name);
 }
