980f6ddb3454587c7121b0cf88a23bf906dbef3b
jamestheengineer <jamestheengineer@gmail.com>
2026-03-04 18:38:30 -0500

    auto clone types

--- comprehensive_rust/hello_world/src/main.rs
+++ comprehensive_rust/hello_world/src/main.rs
@@ -1,12 +1,9 @@
 // Copyright 2023 Google LLC
 // SPDX-License-Identifier: Apache-2.0
 
-fn say_hello(name: String) {
-    println!("Hello {name}")
-}
-
 fn main() {
-    let name = String::from("Alice");
-    say_hello(name.clone());
-    say_hello(name);
+    let x = 42;
+    let y = x;
+    dbg!(x); // would not be accessible if not Copy
+    dbg!(y);
 }
