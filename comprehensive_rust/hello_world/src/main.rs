906a018d27eeb87e88f5b29d2a5e382954da2e04
jamestheengineer <jamestheengineer@gmail.com>
2026-03-05 18:46:44 -0500

    custom copy types

--- comprehensive_rust/hello_world/src/main.rs
+++ comprehensive_rust/hello_world/src/main.rs
@@ -1,9 +1,12 @@
 // Copyright 2023 Google LLC
 // SPDX-License-Identifier: Apache-2.0
 
+#[derive(Copy, Clone, Debug)]
+struct Point(i32, i32);
+
 fn main() {
-    let x = 42;
-    let y = x;
-    dbg!(x); // would not be accessible if not Copy
-    dbg!(y);
+    let p1 = Point(3, 4);
+    let p2 = p1;
+    println!("p1: {p1:?}");
+    println!("p2: {p2:?}");
 }
