34fa1d68d47f8dd802892624fe84e2592f5baa39
jamestheengineer <jamestheengineer@gmail.com>
2026-03-06 07:25:01 -0500

    dropping

--- comprehensive_rust/hello_world/src/main.rs
+++ comprehensive_rust/hello_world/src/main.rs
@@ -1,12 +1,27 @@
 // Copyright 2023 Google LLC
 // SPDX-License-Identifier: Apache-2.0
 
-#[derive(Copy, Clone, Debug)]
-struct Point(i32, i32);
+struct Droppable {
+    name: &'static str,
+}
+
+impl Drop for Droppable {
+    fn drop(&mut self) {
+        println!("Dropping {}", self.name);
+    }
+}
 
 fn main() {
-    let p1 = Point(3, 4);
-    let p2 = p1;
-    println!("p1: {p1:?}");
-    println!("p2: {p2:?}");
+    let a = Droppable { name: "a" };
+    {
+        let b = Droppable { name: "b" };
+        {
+            let c = Droppable { name: "c" };
+            let d = Droppable { name: "d" };
+            println!("Exiting innermost block");
+        }
+        println!("Exiting next block");
+    }
+    drop(a);
+    println!("Exiting main");
 }
