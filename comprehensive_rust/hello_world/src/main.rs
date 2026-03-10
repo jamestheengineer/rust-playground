6a780b7067130b415b0a3e8ae8f19c424ea43b88
jamestheengineer <jamestheengineer@gmail.com>
2026-03-10 18:34:31 -0400

    lists of different size things

--- comprehensive_rust/hello_world/src/main.rs
+++ comprehensive_rust/hello_world/src/main.rs
@@ -1,12 +1,36 @@
-// Copyright 2023 Google LLC
+ // Copyright 2024 Google LLC
 // SPDX-License-Identifier: Apache-2.0
 
-use std::rc::Rc;
+struct Dog {
+    name: String,
+    age: i8,
+}
+struct Cat {
+    lives: i8,
+}
+
+trait Pet {
+    fn talk(&self) -> String;
+}
+
+impl Pet for Dog {
+    fn talk(&self) -> String {
+        format!("Woof, my name is {}!", self.name)
+    }
+}
+
+impl Pet for Cat {
+    fn talk(&self) -> String {
+        String::from("Miau!")
+    }
+}
 
 fn main() {
-    let a = Rc::new(10);
-    let b = Rc::clone(&a);
-
-    dbg!(a);
-    dbg!(b);
+    let pets: Vec<Box<dyn Pet>> = vec![
+        Box::new(Cat { lives: 9 }),
+        Box::new(Dog { name: String::from("Fido"), age: 5 }),
+    ];
+    for pet in pets {
+        println!("Hello, who are you? {}", pet.talk());
+    }
 }
