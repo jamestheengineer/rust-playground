0ae6675869f896b616b022024855db0d38427ca4
jamestheengineer <jamestheengineer@gmail.com>
2026-03-11 19:30:46 -0400

    binary tree

--- comprehensive_rust/hello_world/src/main.rs
+++ comprehensive_rust/hello_world/src/main.rs
@@ -1,36 +1,132 @@
- // Copyright 2024 Google LLC
+// Copyright 2023 Google LLC
 // SPDX-License-Identifier: Apache-2.0
 
-struct Dog {
-    name: String,
-    age: i8,
-}
-struct Cat {
-    lives: i8,
+use std::cmp::Ordering;
+
+/// A node in the binary tree.
+#[derive(Debug)]
+struct Node<T: Ord> {
+    value: T,
+    left: Subtree<T>,
+    right: Subtree<T>,
 }
 
-trait Pet {
-    fn talk(&self) -> String;
+/// A possibly-empty subtree.
+#[derive(Debug)]
+struct Subtree<T: Ord>(Option<Box<Node<T>>>);
+
+/// A container storing a set of values, using a binary tree.
+///
+/// If the same value is added multiple times, it is only stored once.
+#[derive(Debug)]
+pub struct BinaryTree<T: Ord> {
+    root: Subtree<T>,
 }
 
-impl Pet for Dog {
-    fn talk(&self) -> String {
-        format!("Woof, my name is {}!", self.name)
+impl<T: Ord> BinaryTree<T> {
+    fn new() -> Self {
+        Self { root: Subtree::new() }
+    }
+
+    fn insert(&mut self, value: T) {
+        self.root.insert(value);
+    }
+
+    fn has(&self, value: &T) -> bool {
+        self.root.has(value)
+    }
+
+    fn len(&self) -> usize {
+        self.root.len()
     }
 }
 
-impl Pet for Cat {
-    fn talk(&self) -> String {
-        String::from("Miau!")
+impl<T: Ord> Subtree<T> {
+    fn new() -> Self {
+        Self(None)
+    }
+
+    fn insert(&mut self, value: T) {
+        match &mut self.0 {
+            None => self.0 = Some(Box::new(Node::new(value))),
+            Some(n) => match value.cmp(&n.value) {
+                Ordering::Less => n.left.insert(value),
+                Ordering::Equal => {}
+                Ordering::Greater => n.right.insert(value),
+            },
+        }
+    }
+
+    fn has(&self, value: &T) -> bool {
+        match &self.0 {
+            None => false,
+            Some(n) => match value.cmp(&n.value) {
+                Ordering::Less => n.left.has(value),
+                Ordering::Equal => true,
+                Ordering::Greater => n.right.has(value),
+            },
+        }
+    }
+
+    fn len(&self) -> usize {
+        match &self.0 {
+            None => 0,
+            Some(n) => 1 + n.left.len() + n.right.len(),
+        }
     }
 }
 
-fn main() {
-    let pets: Vec<Box<dyn Pet>> = vec![
-        Box::new(Cat { lives: 9 }),
-        Box::new(Dog { name: String::from("Fido"), age: 5 }),
-    ];
-    for pet in pets {
-        println!("Hello, who are you? {}", pet.talk());
+impl<T: Ord> Node<T> {
+    fn new(value: T) -> Self {
+        Self { value, left: Subtree::new(), right: Subtree::new() }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn len() {
+        let mut tree = BinaryTree::new();
+        assert_eq!(tree.len(), 0);
+        tree.insert(2);
+        assert_eq!(tree.len(), 1);
+        tree.insert(1);
+        assert_eq!(tree.len(), 2);
+        tree.insert(2); // not a unique item
+        assert_eq!(tree.len(), 2);
+        tree.insert(3);
+        assert_eq!(tree.len(), 3);
+    }
+
+    #[test]
+    fn has() {
+        let mut tree = BinaryTree::new();
+        fn check_has(tree: &BinaryTree<i32>, exp: &[bool]) {
+            let got: Vec<bool> =
+                (0..exp.len()).map(|i| tree.has(&(i as i32))).collect();
+            assert_eq!(&got, exp);
+        }
+
+        check_has(&tree, &[false, false, false, false, false]);
+        tree.insert(0);
+        check_has(&tree, &[true, false, false, false, false]);
+        tree.insert(4);
+        check_has(&tree, &[true, false, false, false, true]);
+        tree.insert(4);
+        check_has(&tree, &[true, false, false, false, true]);
+        tree.insert(3);
+        check_has(&tree, &[true, false, false, true, true]);
+    }
+
+    #[test]
+    fn unbalanced() {
+        let mut tree = BinaryTree::new();
+        for i in 0..100 {
+            tree.insert(i);
+        }
+        assert_eq!(tree.len(), 100);
+        assert!(tree.has(&50));
     }
 }
