72a481f8024a5749386c786c7d57d195fbb32677
jamestheengineer <jamestheengineer@gmail.com>
2026-03-18 20:26:23 -0400

    borrows

--- comprehensive_rust/hello_world/src/main.rs
+++ comprehensive_rust/hello_world/src/main.rs
@@ -1,123 +1,16 @@
-// Copyright 2023 Google LLC
+// Copyright 2025 Google LLC
 // SPDX-License-Identifier: Apache-2.0
 
-struct Spell {
-    name: String,
-    cost: u32,
-    uses: u32,
-}
-
-struct Wizard {
-    spells: Vec<Spell>,
-    mana: u32,
-}
-
-impl Wizard {
-    fn new(mana: u32) -> Self {
-        Wizard { spells: vec![], mana }
-    }
-
-    fn add_spell(&mut self, spell: Spell) {
-        self.spells.push(spell);
-    }
-
-    fn cast_spell(&mut self, name: &str) {
-        let mut spell_idx = None;
-        for idx in 0..self.spells.len() {
-            if self.spells[idx].name == name {
-                spell_idx = Some(idx);
-                break;
-            }
-        }
-
-        let Some(idx) = spell_idx else {
-            println!("Spell {} not found!", name);
-            return;
-        };
-
-        let spell = &mut self.spells[idx];
-        if self.mana >= spell.cost {
-            self.mana -= spell.cost;
-            spell.uses -= 1;
-            println!(
-                "Casting {}! Mana left: {}. Uses left: {}",
-                spell.name, self.mana, spell.uses
-            );
-            if spell.uses == 0 {
-                self.spells.remove(idx);
-            }
-        } else {
-            println!("Not enough mana to cast {}!", spell.name);
-        }
-    }
+fn borrows(x: &i32) {
+    dbg!(x);
 }
 
 fn main() {
-    let mut merlin = Wizard::new(35);
-    let fireball = Spell { name: String::from("Fireball"), cost: 10, uses: 2 };
-    let ice_blast = Spell { name: String::from("Ice Blast"), cost: 15, uses: 1 };
+    let mut val = 123;
 
-    merlin.add_spell(fireball);
-    merlin.add_spell(ice_blast);
+    // Borrow `val` for the function call.
+    borrows(&val);
 
-    merlin.cast_spell("Fireball"); // Casts successfully
-    merlin.cast_spell("Ice Blast"); // Casts successfully, then removed
-    merlin.cast_spell("Ice Blast"); // Fails (not found)
-    merlin.cast_spell("Fireball"); // Casts successfully, then removed
-    merlin.cast_spell("Fireball"); // Fails (not found)
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_add_spell() {
-        let mut wizard = Wizard::new(10);
-        let spell = Spell { name: String::from("Fireball"), cost: 5, uses: 3 };
-        wizard.add_spell(spell);
-        assert_eq!(wizard.spells.len(), 1);
-    }
-
-    #[test]
-    fn test_cast_spell() {
-        let mut wizard = Wizard::new(10);
-        let spell = Spell { name: String::from("Fireball"), cost: 5, uses: 3 };
-        wizard.add_spell(spell);
-
-        wizard.cast_spell("Fireball");
-        assert_eq!(wizard.mana, 5);
-        assert_eq!(wizard.spells.len(), 1);
-        assert_eq!(wizard.spells[0].uses, 2);
-    }
-
-    #[test]
-    fn test_cast_spell_insufficient_mana() {
-        let mut wizard = Wizard::new(10);
-        let spell = Spell { name: String::from("Fireball"), cost: 15, uses: 3 };
-        wizard.add_spell(spell);
-
-        wizard.cast_spell("Fireball");
-        assert_eq!(wizard.mana, 10);
-        assert_eq!(wizard.spells.len(), 1);
-        assert_eq!(wizard.spells[0].uses, 3);
-    }
-
-    #[test]
-    fn test_cast_spell_not_found() {
-        let mut wizard = Wizard::new(10);
-        wizard.cast_spell("Fireball");
-        assert_eq!(wizard.mana, 10);
-    }
-
-    #[test]
-    fn test_cast_spell_removal() {
-        let mut wizard = Wizard::new(10);
-        let spell = Spell { name: String::from("Fireball"), cost: 5, uses: 1 };
-        wizard.add_spell(spell);
-
-        wizard.cast_spell("Fireball");
-        assert_eq!(wizard.mana, 5);
-        assert_eq!(wizard.spells.len(), 0);
-    }
+    // Borrow has ended and we're free to mutate.
+    val += 5;
 }
