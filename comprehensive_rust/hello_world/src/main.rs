9c4b3d3be888adb3cb0ca12e9f11fe58485e6bc7
jamestheengineer <jamestheengineer@gmail.com>
2026-03-21 08:59:37 -0400

    lifetimes

--- comprehensive_rust/hello_world/src/main.rs
+++ comprehensive_rust/hello_world/src/main.rs
@@ -1,20 +1,20 @@
 // Copyright 2025 Google LLC
 // SPDX-License-Identifier: Apache-2.0
 
-fn multiple(a: &i32, b: &i32) -> &i32 {
-    todo!("Return either `a` or `b`")
+fn pick<'a>(c: bool, a: &'a i32, b: &'a i32) -> &'a i32 {
+    if c { a } else { b }
 }
 
 fn main() {
     let mut a = 5;
     let mut b = 10;
 
-    let r = multiple(&a, &b);
+    let r = pick(true, &a, &b);
 
     // Which one is still borrowed?
     // Should either mutation be allowed?
-    a += 7;
-    b += 7;
+    // a += 7;
+    // b += 7;
 
     dbg!(r);
 }
