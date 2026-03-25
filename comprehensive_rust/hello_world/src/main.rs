debb61a7e8e364695e8af2018ec58a5b67c29151
jamestheengineer <jamestheengineer@gmail.com>
2026-03-24 20:08:40 -0400

    more borrows

--- comprehensive_rust/hello_world/src/main.rs
+++ comprehensive_rust/hello_world/src/main.rs
@@ -1,20 +1,39 @@
 // Copyright 2025 Google LLC
 // SPDX-License-Identifier: Apache-2.0
 
-fn pick<'a>(c: bool, a: &'a i32, b: &'a i32) -> &'a i32 {
-    if c { a } else { b }
+#[derive(Debug)]
+struct Point(i32, i32);
+
+/// Searches `points` for the point closest to `query`.
+/// Assumes there's at least one point in `points`.
+fn find_nearest<'a>(points: &'a [Point], query: &Point) -> &'a Point {
+    fn cab_distance(p1: &Point, p2: &Point) -> i32 {
+        (p1.0 - p2.0).abs() + (p1.1 - p2.1).abs()
+    }
+
+    let mut nearest = None;
+    for p in points {
+        if let Some((_, nearest_dist)) = nearest {
+            let dist = cab_distance(p, query);
+            if dist < nearest_dist {
+                nearest = Some((p, dist));
+            }
+        } else {
+            nearest = Some((p, cab_distance(p, query)));
+        };
+    }
+
+    nearest.map(|(p, _)| p).unwrap()
+    // query // What happens if we do this instead?
 }
 
 fn main() {
-    let mut a = 5;
-    let mut b = 10;
+    let points = &[Point(0, 0), Point(1, 0), Point(-1, 0), Point(0, -1)];
+    let query = Point(0, 2);
+    let nearest = find_nearest(points, &query);
 
-    let r = pick(true, &a, &b);
+    // `query` isn't borrowed at this point.
+    drop(query);
 
-    // Which one is still borrowed?
-    // Should either mutation be allowed?
-    // a += 7;
-    // b += 7;
-
-    dbg!(r);
+    dbg!(nearest);
 }
