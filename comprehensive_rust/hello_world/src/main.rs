3f990fb09dbcc7ec6053d6d1cf28ebb6b76e1d0f
jamestheengineer <jamestheengineer@gmail.com>
2026-03-25 18:42:02 -0400

    more lifetimes

--- comprehensive_rust/hello_world/src/main.rs
+++ comprehensive_rust/hello_world/src/main.rs
@@ -1,39 +1,23 @@
-// Copyright 2025 Google LLC
+// Copyright 2024 Google LLC
 // SPDX-License-Identifier: Apache-2.0
 
 #[derive(Debug)]
-struct Point(i32, i32);
+enum HighlightColor {
+    Pink,
+    Yellow,
+}
 
-/// Searches `points` for the point closest to `query`.
-/// Assumes there's at least one point in `points`.
-fn find_nearest<'a>(points: &'a [Point], query: &Point) -> &'a Point {
-    fn cab_distance(p1: &Point, p2: &Point) -> i32 {
-        (p1.0 - p2.0).abs() + (p1.1 - p2.1).abs()
-    }
-
-    let mut nearest = None;
-    for p in points {
-        if let Some((_, nearest_dist)) = nearest {
-            let dist = cab_distance(p, query);
-            if dist < nearest_dist {
-                nearest = Some((p, dist));
-            }
-        } else {
-            nearest = Some((p, cab_distance(p, query)));
-        };
-    }
-
-    nearest.map(|(p, _)| p).unwrap()
-    // query // What happens if we do this instead?
+#[derive(Debug)]
+struct Highlight<'document> {
+    slice: &'document str,
+    color: HighlightColor,
 }
 
 fn main() {
-    let points = &[Point(0, 0), Point(1, 0), Point(-1, 0), Point(0, -1)];
-    let query = Point(0, 2);
-    let nearest = find_nearest(points, &query);
-
-    // `query` isn't borrowed at this point.
-    drop(query);
-
-    dbg!(nearest);
+    let doc = String::from("The quick brown fox jumps over the lazy dog.");
+    let noun = Highlight { slice: &doc[16..19], color: HighlightColor::Yellow };
+    let verb = Highlight { slice: &doc[20..25], color: HighlightColor::Pink };
+    // drop(doc);
+    dbg!(noun);
+    dbg!(verb);
 }
