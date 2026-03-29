51ec8b9629114392f727633bd91c56d58b34441c
jamestheengineer <jamestheengineer@gmail.com>
2026-03-29 08:30:34 -0400

    iterators

--- comprehensive_rust/hello_world/src/main.rs
+++ comprehensive_rust/hello_world/src/main.rs
@@ -1,10 +1,29 @@
-// Copyright 2024 Google LLC
+// Copyright 2023 Google LLC
 // SPDX-License-Identifier: Apache-2.0
+
+struct SliceIter<'s> {
+    slice: &'s [i32],
+    i: usize,
+}
+
+impl<'s> Iterator for SliceIter<'s> {
+    type Item = &'s i32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.i == self.slice.len() {
+            None
+        } else {
+            let next = &self.slice[self.i];
+            self.i += 1;
+            Some(next)
+        }
+    }
+}
+
 fn main() {
-    let array = [2, 4, 6, 8];
-    let mut i = 0;
-    while i < array.len() {
-        let elem = array[i];
-        i += 1;
+    let slice = &[2, 4, 6, 8];
+    let iter = SliceIter { slice, i: 0 };
+    for elem in iter {
+        dbg!(elem);
     }
 }
