ecf368136afc0248fd62d5029d0301408f4c8a84
jamestheengineer <jamestheengineer@gmail.com>
2026-03-30 20:17:21 -0400

    iterator helpers

--- comprehensive_rust/hello_world/src/main.rs
+++ comprehensive_rust/hello_world/src/main.rs
@@ -1,29 +1,11 @@
-// Copyright 2023 Google LLC
+// Copyright 2024 Google LLC
 // SPDX-License-Identifier: Apache-2.0
 
-struct SliceIter<'s> {
-    slice: &'s [i32],
-    i: usize,
-}
-
-impl<'s> Iterator for SliceIter<'s> {
-    type Item = &'s i32;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.i == self.slice.len() {
-            None
-        } else {
-            let next = &self.slice[self.i];
-            self.i += 1;
-            Some(next)
-        }
-    }
-}
-
 fn main() {
-    let slice = &[2, 4, 6, 8];
-    let iter = SliceIter { slice, i: 0 };
-    for elem in iter {
-        dbg!(elem);
-    }
+    let result: i32 = (1..=10) // Create a range from 1 to 10
+        .filter(|x| x % 2 == 0) // Keep only even numbers
+        .map(|x| x * x) // Square each number
+        .sum(); // Sum up all the squared numbers
+
+    println!("The sum of squares of even numbers from 1 to 10 is: {}", result);
 }
