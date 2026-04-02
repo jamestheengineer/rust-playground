93cf5029daa418e1fbf9998c1df3ccb03c3c0efd
jamestheengineer <jamestheengineer@gmail.com>
2026-04-02 18:32:03 -0400

    collect other way

--- comprehensive_rust/hello_world/src/main.rs
+++ comprehensive_rust/hello_world/src/main.rs
@@ -4,6 +4,6 @@
 fn main() {
     let primes = vec![2, 3, 5, 7];
     println!("primes: {primes:?}");
-    let prime_squares = primes.into_iter().map(|p| p * p).collect::<Vec<_>>();
+    let prime_squares: Vec<_> = primes.into_iter().map(|p| p * p).collect();
     println!("prime_squares: {prime_squares:?}");
 }
