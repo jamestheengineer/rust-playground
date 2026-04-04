2d953330cca30d20f2d96b495e204a1f8557dd14
jamestheengineer <jamestheengineer@gmail.com>
2026-04-03 20:24:26 -0400

    intoiter

--- comprehensive_rust/hello_world/src/main.rs
+++ comprehensive_rust/hello_world/src/main.rs
@@ -1,9 +1,45 @@
-// Copyright 2024 Google LLC
+// Copyright 2023 Google LLC
 // SPDX-License-Identifier: Apache-2.0
 
+struct Grid {
+    x_coords: Vec<u32>,
+    y_coords: Vec<u32>,
+}
+
+impl IntoIterator for Grid {
+    type Item = (u32, u32);
+    type IntoIter = GridIter;
+    fn into_iter(self) -> GridIter {
+        GridIter { grid: self, i: 0, j: 0 }
+    }
+}
+
+struct GridIter {
+    grid: Grid,
+    i: usize,
+    j: usize,
+}
+
+impl Iterator for GridIter {
+    type Item = (u32, u32);
+
+    fn next(&mut self) -> Option<(u32, u32)> {
+        if self.i >= self.grid.x_coords.len() {
+            self.i = 0;
+            self.j += 1;
+            if self.j >= self.grid.y_coords.len() {
+                return None;
+            }
+        }
+        let res = Some((self.grid.x_coords[self.i], self.grid.y_coords[self.j]));
+        self.i += 1;
+        res
+    }
+}
+
 fn main() {
-    let primes = vec![2, 3, 5, 7];
-    println!("primes: {primes:?}");
-    let prime_squares: Vec<_> = primes.into_iter().map(|p| p * p).collect();
-    println!("prime_squares: {prime_squares:?}");
+    let grid = Grid { x_coords: vec![3, 5, 7, 9], y_coords: vec![10, 20, 30, 40] };
+    for (x, y) in grid {
+        println!("point = {x}, {y}");
+    }
 }
