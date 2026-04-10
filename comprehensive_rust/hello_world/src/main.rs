b3e541b3622d3ab461e9f37065a6a9e0d2295a72
jamestheengineer <jamestheengineer@gmail.com>
2026-04-09 20:46:51 -0400

    private fields

--- comprehensive_rust/hello_world/src/main.rs
+++ comprehensive_rust/hello_world/src/main.rs
@@ -1,27 +1,34 @@
-// Copyright 2022 Google LLC
+// Copyright 2024 Google LLC
 // SPDX-License-Identifier: Apache-2.0
 
+use outer::Foo;
+
 mod outer {
-    fn private() {
-        println!("outer::private");
+    pub struct Foo {
+        pub val: i32,
+        is_big: bool,
     }
 
-    pub fn public() {
-        println!("outer::public");
-    }
-
-    mod inner {
-        fn private() {
-            println!("outer::inner::private");
+    impl Foo {
+        pub fn new(val: i32) -> Self {
+            Self { val, is_big: val > 100 }
         }
+    }
 
-        pub fn public() {
-            println!("outer::inner::public");
-            super::private();
+    pub mod inner {
+        use super::Foo;
+
+        pub fn print_foo(foo: &Foo) {
+            println!("Is {} big? {}", foo.val, foo.is_big);
         }
     }
 }
 
 fn main() {
-    outer::public();
+    let foo = Foo::new(42);
+    println!("foo.val = {}", foo.val);
+    // let foo = Foo { val: 42, is_big: true };
+
+    outer::inner::print_foo(&foo);
+    // println!("Is {} big? {}", foo.val, foo.is_big);
 }
