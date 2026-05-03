5baf50cd996350b1a45424ac7fe5f831b9443d98
jamestheengineer <jamestheengineer@gmail.com>
2026-05-03 08:08:24 -0400

    Results

--- comprehensive_rust/hello_world/src/main.rs
+++ comprehensive_rust/hello_world/src/main.rs
@@ -1,7 +1,22 @@
 // Copyright 2022 Google LLC
 // SPDX-License-Identifier: Apache-2.0
 
+use std::fs::File;
+use std::io::Read;
+
 fn main() {
-    let v = vec![10, 20, 30];
-    dbg!(v[100]);
+    let file: Result<File, std::io::Error> = File::open("diary.txt");
+    match file {
+        Ok(mut file) => {
+            let mut contents = String::new();
+            if let Ok(bytes) = file.read_to_string(&mut contents) {
+                println!("Dear diary: {contents} ({bytes} bytes)");
+            } else {
+                println!("Could not read file content");
+            }
+        }
+        Err(err) => {
+            println!("The diary could not be opened: {err}");
+        }
+    }
 }
