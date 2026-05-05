1d2022d7d282259a6b9c75527234a6a3cb234544
jamestheengineer <jamestheengineer@gmail.com>
2026-05-04 21:02:10 -0400

    better way to match

--- comprehensive_rust/hello_world/src/main.rs
+++ comprehensive_rust/hello_world/src/main.rs
@@ -1,22 +1,20 @@
-// Copyright 2022 Google LLC
+// Copyright 2023 Google LLC
 // SPDX-License-Identifier: Apache-2.0
 
-use std::fs::File;
 use std::io::Read;
+use std::{fs, io};
+
+fn read_username(path: &str) -> Result<String, io::Error> {
+    let username_file_result = fs::File::open(path);
+    let mut username_file = username_file_result ?;
+
+    let mut username = String::new();
+    username_file.read_to_string(&mut username)?;
+    Ok(username)
+}
 
 fn main() {
-    let file: Result<File, std::io::Error> = File::open("diary.txt");
-    match file {
-        Ok(mut file) => {
-            let mut contents = String::new();
-            if let Ok(bytes) = file.read_to_string(&mut contents) {
-                println!("Dear diary: {contents} ({bytes} bytes)");
-            } else {
-                println!("Could not read file content");
-            }
-        }
-        Err(err) => {
-            println!("The diary could not be opened: {err}");
-        }
-    }
+    //fs::write("config.dat", "alice").unwrap();
+    let username = read_username("config.dat");
+    println!("username or error: {username:?}");
 }
