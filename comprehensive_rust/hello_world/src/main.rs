93e7a3db544aaa7f17f71b0e607c67f4dc26ff54
jamestheengineer <jamestheengineer@gmail.com>
2026-05-06 17:39:04 -0400

    dyn error, don't use these for APIs

--- comprehensive_rust/hello_world/src/main.rs
+++ comprehensive_rust/hello_world/src/main.rs
@@ -2,43 +2,20 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use std::error::Error;
+use std::fs;
 use std::io::Read;
-use std::{fmt, fs, io};
 
-#[derive(Debug)]
-enum ReadUsernameError {
-    IoError(io::Error),
-    EmptyUsername(String),
-}
-
-impl Error for ReadUsernameError {}
-
-impl fmt::Display for ReadUsernameError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Self::IoError(e) => write!(f, "I/O error: {e}"),
-            Self::EmptyUsername(path) => write!(f, "Found no username in {path}"),
-        }
-    }
-}
-
-impl From<io::Error> for ReadUsernameError {
-    fn from(err: io::Error) -> Self {
-        Self::IoError(err)
-    }
-}
-
-fn read_username(path: &str) -> Result<String, ReadUsernameError> {
-    let mut username = String::with_capacity(100);
-    fs::File::open(path)?.read_to_string(&mut username)?;
-    if username.is_empty() {
-        return Err(ReadUsernameError::EmptyUsername(String::from(path)));
-    }
-    Ok(username)
+fn read_count(path: &str) -> Result<i32, Box<dyn Error>> {
+    let mut count_str = String::new();
+    fs::File::open(path)?.read_to_string(&mut count_str)?;
+    let count: i32 = count_str.parse()?;
+    Ok(count)
 }
 
 fn main() {
-    //std::fs::write("config.dat", "").unwrap();
-    let username = read_username("config.dat");
-    println!("username or error: {username:?}");
+    fs::write("count.dat", "1i3").unwrap();
+    match read_count("count.dat") {
+        Ok(count) => println!("Count: {count}"),
+        Err(err) => println!("Error: {err}"),
+    }
 }
