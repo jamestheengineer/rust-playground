8f70129cb72c37cc930e5b8a0d5da14096c9edd1
jamestheengineer <jamestheengineer@gmail.com>
2026-07-11 07:20:27 -0400

    scoped threads to borrow

--- comprehensive_rust/hello_world/src/main.rs
+++ comprehensive_rust/hello_world/src/main.rs
@@ -2,18 +2,16 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use std::thread;
-use std::time::Duration;
+
+fn foo() {
+    let s = String::from("Hello");
+    thread::scope(|scope| {
+        scope.spawn(|| {
+            dbg!(s.len());
+        });
+    });
+}
 
 fn main() {
-    thread::spawn(|| {
-        for i in 0..10 {
-            println!("Count in thread: {i}!");
-            thread::sleep(Duration::from_millis(5));
-        }
-    });
-
-    for i in 0..5 {
-        println!("Main thread: {i}");
-        thread::sleep(Duration::from_millis(5));
-    }
+    foo();
 }
